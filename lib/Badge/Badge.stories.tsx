b34071fcb6b17ecfdc0d2c7552113690e0eb5d75
gdominguez210 <gdominguez210@live.com>
2024-09-02 16:15:27 -0400

    docs(lib/Badge): add additional stories for variants and sizes

--- lib/Badge/Badge.stories.tsx
+++ lib/Badge/Badge.stories.tsx
@@ -1,15 +1,70 @@
 /* eslint-disable import/no-extraneous-dependencies, react/jsx-props-no-spreading */
 import type { StoryObj, Meta } from '@storybook/react';
-import { Badge as BadgeComponent } from  '@lib/Badge';
+import { Badge as BadgeComponent } from '@lib/Badge';
 
 export default {
   title: 'components/Badge',
   component: BadgeComponent,
-  // parameters: { layout: "fullscreen" },
 } as Meta<typeof BadgeComponent>;
 
 export const Badge: StoryObj<typeof BadgeComponent> = {
   args: {
-    children: 'Label Text'
-  }
+    children: 'Label',
+  },
+};
+
+export const BadgeNeutral: StoryObj<typeof BadgeComponent> = {
+  args: {
+    children: 'Label',
+    variant: 'neutral',
+  },
+};
+
+export const BadgeDanger: StoryObj<typeof BadgeComponent> = {
+  args: {
+    children: 'Label',
+    variant: 'danger',
+  },
+};
+
+export const BadgeWarning: StoryObj<typeof BadgeComponent> = {
+  args: {
+    children: 'Label',
+    variant: 'warning',
+  },
+};
+
+export const BadgeSuccess: StoryObj<typeof BadgeComponent> = {
+  args: {
+    children: 'Label',
+    variant: 'success',
+  },
+};
+
+export const BadgeBrand: StoryObj<typeof BadgeComponent> = {
+  args: {
+    children: 'Label',
+    variant: 'brand',
+  },
+};
+
+export const BadgeSmall: StoryObj<typeof BadgeComponent> = {
+  args: {
+    children: 'Label',
+    size: 'sm',
+  },
+};
+
+export const BadgeMedium: StoryObj<typeof BadgeComponent> = {
+  args: {
+    children: 'Label',
+    size: 'md',
+  },
+};
+
+export const BadgeLarge: StoryObj<typeof BadgeComponent> = {
+  args: {
+    children: 'Label',
+    size: 'lg',
+  },
 };
