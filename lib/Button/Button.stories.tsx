d8af23f7b0d748033788a1cdd51f9ad3fe6a5f38
gdominguez210 <gdominguez210@live.com>
2024-09-06 00:47:44 -0400

    docs(lib/Button/Button.stories.tsx): create Grid components, and refactor stories to use Grid components

--- lib/Button/Button.stories.tsx
+++ lib/Button/Button.stories.tsx
@@ -3,16 +3,16 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button as ButtonComponent, type ButtonProps } from './Button';
 import { Icon } from '@lib/Icon';
 import { sizes } from './Button';
+import type { HTMLAttributes } from 'react';
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-const gridHeaderRows = ['Medium', 'Large', 'Extra Large', '2 Extra Large'];
-const gridBodyRows = [
-  {
-    label: 'Normal',
-    render: (props: ButtonProps) => <ButtonComponent {...props}>Button CTA</ButtonComponent>,
-  },
-  {
-    label: 'Hover',
-    render: (props: ButtonProps) => (
+const gridHeaderCols = ['Medium', 'Large', 'Extra Large', '2 Extra Large'];
+const gridBodyRows: [string, (props: ButtonProps) => JSX.Element][] = [
+  ['Normal', (props: ButtonProps) => <ButtonComponent {...props}>Button CTA</ButtonComponent>],
+  [
+    'Hover',
+    (props) => (
       <ButtonComponent
         className="pseudo-hover"
         {...props}
@@ -20,10 +20,10 @@ const gridBodyRows = [
         Button CTA
       </ButtonComponent>
     ),
-  },
-  {
-    label: 'Focus',
-    render: (props: ButtonProps) => (
+  ],
+  [
+    'Focus',
+    (props) => (
       <ButtonComponent
         className="pseudo-focus"
         {...props}
@@ -31,10 +31,10 @@ const gridBodyRows = [
         Button CTA
       </ButtonComponent>
     ),
-  },
-  {
-    label: 'Disabled',
-    render: (props: ButtonProps) => (
+  ],
+  [
+    'Disabled',
+    (props) => (
       <ButtonComponent
         disabled
         {...props}
@@ -42,19 +42,19 @@ const gridBodyRows = [
         Button CTA
       </ButtonComponent>
     ),
-  },
-  {
-    label: 'With Icon',
-    render: (props: ButtonProps) => (
+  ],
+  [
+    'With Icon',
+    (props) => (
       <ButtonComponent {...props}>
         <Icon name="star-line" />
         Button CTA
       </ButtonComponent>
     ),
-  },
-  {
-    label: 'Icon Only',
-    render: (props: ButtonProps) => (
+  ],
+  [
+    'Icon Only',
+    (props) => (
       <ButtonComponent
         aria-label="Button CTA"
         iconOnly
@@ -63,29 +63,83 @@ const gridBodyRows = [
         <Icon name="star-line" />
       </ButtonComponent>
     ),
-  },
+  ],
 ];
 
-const gridStyles = {
-  rows: {
-    '1': 'row-start-1',
-    '2': 'row-start-2',
-    '3': 'row-start-3',
-    '4': 'row-start-4',
-    '5': 'row-start-5',
-    '6': 'row-start-6',
-    '7': 'row-start-7',
+const gridStartStyles = {
+  rows: [
+    'row-start-1',
+    'row-start-2',
+    'row-start-3',
+    'row-start-4',
+    'row-start-5',
+    'row-start-6',
+    'row-start-7',
+  ],
+  cols: [
+    'col-start-1',
+    'col-start-2',
+    'col-start-3',
+    'col-start-4',
+    'col-start-5',
+    'col-start-6',
+    'col-start-7',
+  ],
+};
+
+const buttonVariantArgTypes = {
+  children: {
+    table: { disable: true },
   },
-  cols: {
-    '1': 'col-start-1',
-    '2': 'col-start-2',
-    '3': 'col-start-3',
-    '4': 'col-start-4',
-    '5': 'col-start-5',
-    '6': 'col-start-6',
-    '7': 'col-start-7',
+  variant: {
+    table: { disable: true },
+  },
+  size: {
+    table: { disable: true },
+  },
+  iconOnly: {
+    table: { disable: true },
   },
 };
+
+interface GridProps extends HTMLAttributes<HTMLElement> {}
+
+function Grid(props: GridProps) {
+  const { children, className } = props;
+
+  return (
+    <div className={twMerge(clsx('grid grid-cols-5 grid-rows-7 gap-3', className))}>{children}</div>
+  );
+}
+
+interface GridItemProps extends HTMLAttributes<HTMLDivElement> {
+  index?: number;
+  colOffset?: number;
+  rowOffset?: number;
+}
+
+function GridItem(props: GridItemProps) {
+  const { children, className, index, rowOffset, colOffset, ...restProps } = props;
+
+  const rowStyle =
+    typeof index === 'number' && typeof rowOffset === 'number'
+      ? gridStartStyles.rows[index + rowOffset]
+      : null;
+
+  const colStyle =
+    typeof index === 'number' && typeof colOffset === 'number'
+      ? gridStartStyles.cols[index + colOffset]
+      : null;
+
+  return (
+    <div
+      className={clsx('font-medium', rowStyle, colStyle, className)}
+      {...restProps}
+    >
+      {children}
+    </div>
+  );
+}
 
 export default {
   title: 'components/Button',
@@ -100,6 +154,8 @@ export const ButtonSandbox: StoryObj<typeof ButtonComponent> = {
     },
   },
   args: {
+    variant: 'primary',
+    size: 'md',
     children: <>Button CTA</>,
   },
 };
@@ -107,49 +163,33 @@ export const ButtonSandbox: StoryObj<typeof ButtonComponent> = {
 export const PrimaryButtons: StoryObj<typeof ButtonComponent> = {
   args: {},
   argTypes: {
-    children: {
-      table: { disable: true },
-    },
-    variant: {
-      table: { disable: true },
-    },
-    size: {
-      table: { disable: true },
-    },
-    iconOnly: {
-      table: { disable: true },
-    },
+    ...buttonVariantArgTypes,
   },
   render: () => {
     return (
-      <div className="grid grid-cols-5 grid-rows-7 gap-3">
-        {gridHeaderRows.map((row, i) => (
-          <div
-            className={`${gridStyles.cols[String(i + 2) as keyof typeof gridStyles.cols]} font-medium`}
+      <Grid>
+        {gridHeaderCols.map((col, i) => (
+          <GridItem
+            index={i}
+            colOffset={1}
           >
-            {row}
-          </div>
+            {col}
+          </GridItem>
         ))}
-        {gridBodyRows.map((row, i) => {
-          return (
-            <>
-              <div
-                className={`${gridStyles.rows[String(i + 2) as keyof typeof gridStyles.rows]} font-medium`}
-              >
-                {row.label}
-              </div>
-              {sizes.map((size) => (
-                <div
-                  className={gridStyles.rows[String(i + 2) as keyof typeof gridStyles.rows]}
-                  key={`${row.label}-${size}`}
-                >
-                  {row.render({ size, variant: 'primary' })}
-                </div>
-              ))}
-            </>
-          );
-        })}
-      </div>
+        {gridBodyRows.map(([label, button], i) => (
+          <>
+            <GridItem
+              index={i}
+              rowOffset={1}
+            >
+              {label}
+            </GridItem>
+            {sizes.map((size) => (
+              <GridItem>{button({ size, variant: 'primary' })}</GridItem>
+            ))}
+          </>
+        ))}
+      </Grid>
     );
   },
 };
@@ -157,41 +197,33 @@ export const PrimaryButtons: StoryObj<typeof ButtonComponent> = {
 export const SecondaryButtons: StoryObj<typeof ButtonComponent> = {
   args: {},
   argTypes: {
-    children: {
-      table: { disable: true },
-    },
-    variant: {
-      table: { disable: true },
-    },
-    size: {
-      table: { disable: true },
-    },
-    iconOnly: {
-      table: { disable: true },
-    },
+    ...buttonVariantArgTypes,
   },
   render: () => {
     return (
-      <div className="grid grid-cols-5 grid-rows-7 gap-3">
-        {gridHeaderRows.map((row, i) => (
-          <div className={`col-start-${i + 2} font-medium`}>{row}</div>
+      <Grid>
+        {gridHeaderCols.map((col, i) => (
+          <GridItem
+            index={i}
+            colOffset={1}
+          >
+            {col}
+          </GridItem>
         ))}
-        {gridBodyRows.map((row, i) => {
-          return (
-            <>
-              <div className={`row-start-${i + 2} font-medium`}>{row.label}</div>
-              {sizes.map((size) => (
-                <div
-                  className={`row-start-${i + 2}`}
-                  key={`${row.label}-${size}`}
-                >
-                  {row.render({ size, variant: 'secondary' })}
-                </div>
-              ))}
-            </>
-          );
-        })}
-      </div>
+        {gridBodyRows.map(([label, button], i) => (
+          <>
+            <GridItem
+              index={i}
+              rowOffset={1}
+            >
+              {label}
+            </GridItem>
+            {sizes.map((size) => (
+              <GridItem>{button({ size, variant: 'secondary' })}</GridItem>
+            ))}
+          </>
+        ))}
+      </Grid>
     );
   },
 };
@@ -199,41 +231,33 @@ export const SecondaryButtons: StoryObj<typeof ButtonComponent> = {
 export const TertiaryButtons: StoryObj<typeof ButtonComponent> = {
   args: {},
   argTypes: {
-    children: {
-      table: { disable: true },
-    },
-    variant: {
-      table: { disable: true },
-    },
-    size: {
-      table: { disable: true },
-    },
-    iconOnly: {
-      table: { disable: true },
-    },
+    ...buttonVariantArgTypes,
   },
   render: () => {
     return (
-      <div className="grid grid-cols-5 grid-rows-7 gap-3">
-        {gridHeaderRows.map((row, i) => (
-          <div className={`col-start-${i + 2} font-medium`}>{row}</div>
+      <Grid>
+        {gridHeaderCols.map((col, i) => (
+          <GridItem
+            index={i}
+            colOffset={1}
+          >
+            {col}
+          </GridItem>
         ))}
-        {gridBodyRows.map((row, i) => {
-          return (
-            <>
-              <div className={`row-start-${i + 2} font-medium`}>{row.label}</div>
-              {sizes.map((size) => (
-                <div
-                  className={`row-start-${i + 2}`}
-                  key={`${row.label}-${size}`}
-                >
-                  {row.render({ size, variant: 'tertiary' })}
-                </div>
-              ))}
-            </>
-          );
-        })}
-      </div>
+        {gridBodyRows.map(([label, button], i) => (
+          <>
+            <GridItem
+              index={i}
+              rowOffset={1}
+            >
+              {label}
+            </GridItem>
+            {sizes.map((size) => (
+              <GridItem>{button({ size, variant: 'tertiary' })}</GridItem>
+            ))}
+          </>
+        ))}
+      </Grid>
     );
   },
 };
@@ -241,41 +265,33 @@ export const TertiaryButtons: StoryObj<typeof ButtonComponent> = {
 export const DestructiveButtons: StoryObj<typeof ButtonComponent> = {
   args: {},
   argTypes: {
-    children: {
-      table: { disable: true },
-    },
-    variant: {
-      table: { disable: true },
-    },
-    size: {
-      table: { disable: true },
-    },
-    iconOnly: {
-      table: { disable: true },
-    },
+    ...buttonVariantArgTypes,
   },
   render: () => {
     return (
-      <div className="grid grid-cols-5 grid-rows-7 gap-3">
-        {gridHeaderRows.map((row, i) => (
-          <div className={`col-start-${i + 2} font-medium`}>{row}</div>
+      <Grid>
+        {gridHeaderCols.map((col, i) => (
+          <GridItem
+            index={i}
+            colOffset={1}
+          >
+            {col}
+          </GridItem>
         ))}
-        {gridBodyRows.map((row, i) => {
-          return (
-            <>
-              <div className={`row-start-${i + 2} font-medium`}>{row.label}</div>
-              {sizes.map((size) => (
-                <div
-                  className={`row-start-${i + 2}`}
-                  key={`${row.label}-${size}`}
-                >
-                  {row.render({ size, variant: 'destructive' })}
-                </div>
-              ))}
-            </>
-          );
-        })}
-      </div>
+        {gridBodyRows.map(([label, button], i) => (
+          <>
+            <GridItem
+              index={i}
+              rowOffset={1}
+            >
+              {label}
+            </GridItem>
+            {sizes.map((size) => (
+              <GridItem>{button({ size, variant: 'destructive' })}</GridItem>
+            ))}
+          </>
+        ))}
+      </Grid>
     );
   },
 };
@@ -283,41 +299,33 @@ export const DestructiveButtons: StoryObj<typeof ButtonComponent> = {
 export const ColoredLinkButtons: StoryObj<typeof ButtonComponent> = {
   args: {},
   argTypes: {
-    children: {
-      table: { disable: true },
-    },
-    variant: {
-      table: { disable: true },
-    },
-    size: {
-      table: { disable: true },
-    },
-    iconOnly: {
-      table: { disable: true },
-    },
+    ...buttonVariantArgTypes,
   },
   render: () => {
     return (
-      <div className="grid grid-cols-5 grid-rows-7 gap-3">
-        {gridHeaderRows.map((row, i) => (
-          <div className={`col-start-${i + 2} font-medium`}>{row}</div>
+      <Grid>
+        {gridHeaderCols.map((col, i) => (
+          <GridItem
+            index={i}
+            colOffset={1}
+          >
+            {col}
+          </GridItem>
         ))}
-        {gridBodyRows.map((row, i) => {
-          return (
-            <>
-              <div className={`row-start-${i + 2} font-medium`}>{row.label}</div>
-              {sizes.map((size) => (
-                <div
-                  className={`row-start-${i + 2}`}
-                  key={`${row.label}-${size}`}
-                >
-                  {row.render({ size, variant: 'linkColor' })}
-                </div>
-              ))}
-            </>
-          );
-        })}
-      </div>
+        {gridBodyRows.map(([label, button], i) => (
+          <>
+            <GridItem
+              index={i}
+              rowOffset={1}
+            >
+              {label}
+            </GridItem>
+            {sizes.map((size) => (
+              <GridItem>{button({ size, variant: 'linkColor' })}</GridItem>
+            ))}
+          </>
+        ))}
+      </Grid>
     );
   },
 };
@@ -325,41 +333,33 @@ export const ColoredLinkButtons: StoryObj<typeof ButtonComponent> = {
 export const GrayLinkButtons: StoryObj<typeof ButtonComponent> = {
   args: {},
   argTypes: {
-    children: {
-      table: { disable: true },
-    },
-    variant: {
-      table: { disable: true },
-    },
-    size: {
-      table: { disable: true },
-    },
-    iconOnly: {
-      table: { disable: true },
-    },
+    ...buttonVariantArgTypes,
   },
   render: () => {
     return (
-      <div className="grid grid-cols-5 grid-rows-7 gap-3">
-        {gridHeaderRows.map((row, i) => (
-          <div className={`col-start-${i + 2} font-medium`}>{row}</div>
+      <Grid>
+        {gridHeaderCols.map((col, i) => (
+          <GridItem
+            index={i}
+            colOffset={1}
+          >
+            {col}
+          </GridItem>
         ))}
-        {gridBodyRows.map((row, i) => {
-          return (
-            <>
-              <div className={`row-start-${i + 2} font-medium`}>{row.label}</div>
-              {sizes.map((size) => (
-                <div
-                  className={`row-start-${i + 2}`}
-                  key={`${row.label}-${size}`}
-                >
-                  {row.render({ size, variant: 'linkGray' })}
-                </div>
-              ))}
-            </>
-          );
-        })}
-      </div>
+        {gridBodyRows.map(([label, button], i) => (
+          <>
+            <GridItem
+              index={i}
+              rowOffset={1}
+            >
+              {label}
+            </GridItem>
+            {sizes.map((size) => (
+              <GridItem>{button({ size, variant: 'linkGray' })}</GridItem>
+            ))}
+          </>
+        ))}
+      </Grid>
     );
   },
 };
