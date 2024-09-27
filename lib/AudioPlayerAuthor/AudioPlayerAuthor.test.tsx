5ac08de382ced15a80d29a16c7bd507fbff2954f
gdominguez210 <gdominguez210@live.com>
2024-09-26 23:09:28 -0400

    test(lib/AudioPlayerAuthor): add AudioPlayerAuthor unit tests

--- lib/AudioPlayerAuthor/AudioPlayerAuthor.test.tsx
+++ lib/AudioPlayerAuthor/AudioPlayerAuthor.test.tsx
@@ -1,12 +1,61 @@
-
-
-import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
-import { AudioPlayerAuthor } from '@lib/AudioPlayerAuthor';
+import { screen, render, waitFor } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import { AudioPlayerAuthor } from '@lib/AudioPlayerAuthor/AudioPlayerAuthor';
+import { trackData } from '@lib/AudioPlayer/data';
+import {
+  AudioPlayerContextProviderWithTrackData as AudioWrapper,
+  AUDIO_PLAYER_CONTEXT_ERROR,
+} from '@lib/AudioPlayerContextProvider/data';
 
 describe('AudioPlayerAuthor should...', () => {
-  test('it should match the snapshot', () => {
-    const { container } = render(<AudioPlayerAuthor />);
+  test('match the snapshot', () => {
+    const { container } = render(<AudioPlayerAuthor />, { wrapper: AudioWrapper });
+
     expect(container).toMatchSnapshot();
+  });
+
+  test('should throw an error when not used in AudioPlayerContextProvider', async () => {
+    vi.spyOn(console, 'error').mockImplementation(() => vi.fn());
+
+    await waitFor(() =>
+      expect(() => render(<AudioPlayerAuthor />)).toThrow(AUDIO_PLAYER_CONTEXT_ERROR),
+    );
+    vi.restoreAllMocks();
+  });
+
+  test("display a track's author", () => {
+    render(<AudioPlayerAuthor data-testid="audio-player-author" />, { wrapper: AudioWrapper });
+
+    const element = screen.getByTestId('audio-player-author');
+
+    expect(element).toHaveTextContent(trackData[0]?.author as string);
+  });
+
+  test('allow the user to change the returned element type', () => {
+    render(
+      <AudioPlayerAuthor
+        as="span"
+        data-testid="audio-player-author"
+      />,
+      { wrapper: AudioWrapper },
+    );
+
+    const element = screen.getByTestId('audio-player-author');
+
+    expect(element.nodeName.toLowerCase()).toBe('span');
+  });
+
+  test('allow the user to add a className', () => {
+    render(
+      <AudioPlayerAuthor
+        className="text-md"
+        data-testid="audio-player-author"
+      />,
+      { wrapper: AudioWrapper },
+    );
+
+    const element = screen.getByTestId('audio-player-author');
+
+    expect(element).toHaveClass('text-md');
   });
 });
