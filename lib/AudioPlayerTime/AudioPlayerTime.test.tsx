75108b75f248aa74b97264b423e8542f8c3fcef0
gdominguez210 <gdominguez210@live.com>
2024-10-01 00:00:45 -0400

    test(lib/AudioPlayerTime): add unit tests

--- lib/AudioPlayerTime/AudioPlayerTime.test.tsx
+++ lib/AudioPlayerTime/AudioPlayerTime.test.tsx
@@ -1,12 +1,38 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
 
-
-import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import {
+  AUDIO_PLAYER_CONTEXT_ERROR,
+  AudioPlayerContextProviderWithTrackData as AudioWrapper,
+} from '@lib/AudioPlayerContextProvider/data';
 import { AudioPlayerTime } from '@lib/AudioPlayerTime';
 
 describe('AudioPlayerTime should...', () => {
   test('it should match the snapshot', () => {
-    const { container } = render(<AudioPlayerTime />);
+    const { container } = render(<AudioPlayerTime />, { wrapper: AudioWrapper });
     expect(container).toMatchSnapshot();
+  });
+
+  test('throw an error when not used in AudioPlayerContextProvider', async () => {
+    vi.spyOn(console, 'error').mockImplementation(() => vi.fn());
+
+    await waitFor(() =>
+      expect(() => render(<AudioPlayerTime />)).toThrow(AUDIO_PLAYER_CONTEXT_ERROR),
+    );
+    vi.restoreAllMocks();
+  });
+
+  test('allow the user to add a className', () => {
+    render(
+      <AudioPlayerTime
+        className="text-md"
+        data-testid="audio-player-time"
+      />,
+      { wrapper: AudioWrapper },
+    );
+
+    const element = screen.getByTestId('audio-player-time');
+
+    expect(element).toHaveClass('text-md');
   });
 });
