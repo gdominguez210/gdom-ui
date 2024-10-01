9ab9f361ac295d112f30f95b684b954ace254c47
gdominguez210 <gdominguez210@live.com>
2024-09-30 23:35:10 -0400

    test(lib/AudioPlayerInfo): add AudioPlayerInfo unit tests

--- lib/AudioPlayerInfo/AudioPlayerInfo.test.tsx
+++ lib/AudioPlayerInfo/AudioPlayerInfo.test.tsx
@@ -1,10 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
+
 import { AudioPlayerInfo } from '@lib/AudioPlayerInfo';
 
 describe('AudioPlayerInfo should...', () => {
   test('it should match the snapshot', () => {
-    const { container } = render(<AudioPlayerInfo />);
+    const { container } = render(<AudioPlayerInfo>Track Info</AudioPlayerInfo>);
     expect(container).toMatchSnapshot();
+  });
+
+  test('allow the user to add a className', () => {
+    render(
+      <AudioPlayerInfo
+        className="gap-6"
+        data-testid="audio-player-info"
+      >
+        Track Info
+      </AudioPlayerInfo>,
+    );
+
+    const element = screen.getByTestId('audio-player-info');
+
+    expect(element).toHaveClass('gap-6');
   });
 });
