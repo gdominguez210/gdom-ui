68c5dbff9ef99f966bd944d3761d4ee3e1e421ee
gdominguez210 <gdominguez210@live.com>
2025-02-22 13:43:37 -0500

    fix(AudioPlayer): refactor component type to be truly polymorphic

--- lib/AudioPlayer/AudioPlayer.tsx
+++ lib/AudioPlayer/AudioPlayer.tsx
@@ -1,6 +1,7 @@
-import { type HTMLAttributes, type ElementType } from 'react';
 import clsx from 'clsx';
+import { type ComponentPropsWithoutRef, type ElementType } from 'react';
 import { twMerge } from 'tailwind-merge';
+
 import { AudioPlayerAuthor } from '@lib/AudioPlayerAuthor/AudioPlayerAuthor';
 import { AudioPlayerContextProvider } from '@lib/AudioPlayerContextProvider/AudioPlayerContextProvider';
 import { AudioPlayerControls } from '@lib/AudioPlayerControls/AudioPlayerControls';
@@ -11,24 +12,22 @@ import { AudioPlayerTime } from '@lib/AudioPlayerTime/AudioPlayerTime';
 import { AudioPlayerTitle } from '@lib/AudioPlayerTitle/AudioPlayerTitle';
 import { AudioPlayerVolume } from '@lib/AudioPlayerVolume/AudioPlayerVolume';
 
-export interface AudioPlayerProps extends HTMLAttributes<HTMLElement> {
+export type AudioPlayerProps<T extends ElementType = 'div'> = {
   /** @default div */
-  as?: ElementType;
-}
+  as?: T;
+} & ComponentPropsWithoutRef<T>;
 
-export function AudioPlayer(props: AudioPlayerProps) {
-  const { as = 'div', children, className } = props;
-
-  const Node = as;
+export function AudioPlayer<T extends ElementType>(props: AudioPlayerProps<T>) {
+  const { as: Element = 'div', children, className } = props;
 
   return (
-    <Node
+    <Element
       className={twMerge(
         clsx('flex flex-col justify-center bg-slate-700 text-neutral-100', className),
       )}
     >
       {children}
-    </Node>
+    </Element>
   );
 }
 
