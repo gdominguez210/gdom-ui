cbe13fc70408b3bf6a7d614161014ab832f2c84a
gdominguez210 <gdominguez210@live.com>
2025-02-23 21:34:44 -0500

    refactor(useAudioPlayerContext): rename to useAudioPlayerContextState

--- lib/AudioPlayerContextProvider/useAudioPlayerContextState.ts
+++ lib/AudioPlayerContextProvider/useAudioPlayerContextState.ts
@@ -1,12 +1,12 @@
 import { useContext } from 'react';
 import {
-  AudioPlayerContext,
-  type AudioPlayerContextType,
+  AudioPlayerContextState,
+  type AudioPlayerContextStateType,
 } from '@lib/AudioPlayerContextProvider/AudioPlayerContextProvider';
 import { AUDIO_PLAYER_CONTEXT_ERROR } from '@lib/AudioPlayerContextProvider/data';
 
-export function useAudioPlayerContext(): AudioPlayerContextType {
-  const context = useContext(AudioPlayerContext);
+export function useAudioPlayerContextState(): AudioPlayerContextStateType {
+  const context = useContext(AudioPlayerContextState);
 
   if (!context) {
     throw new Error(AUDIO_PLAYER_CONTEXT_ERROR);
