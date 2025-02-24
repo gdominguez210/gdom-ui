b34c53fdad3c3bad86424e3b8b7c75992c861daa
gdominguez210 <gdominguez210@live.com>
2025-02-23 21:32:05 -0500

    refactor(AudioPlayerContextProvider): useReducer, split context into StateContext/DispatchContext

--- lib/AudioPlayerContextProvider/AudioPlayerContextProvider.tsx
+++ lib/AudioPlayerContextProvider/AudioPlayerContextProvider.tsx
@@ -1,13 +1,67 @@
 import {
-  useState,
   useMemo,
   createContext,
   useRef,
+  useReducer,
   type Dispatch,
-  type SetStateAction,
   type RefObject,
   type ReactNode,
 } from 'react';
+
+const AUDIO_PLAYER_ACTIONS = {
+  SET_CURRENT_TRACK_INDEX: 'SET_CURRENT_TRACK_INDEX',
+  SET_CURRENT_TIME: 'SET_CURRENT_TIME',
+  SET_DURATION: 'SET_DURATION',
+  SET_IS_PLAYING: 'SET_IS_PLAYING',
+} as const;
+
+type ActionPayloads = {
+  [AUDIO_PLAYER_ACTIONS.SET_CURRENT_TRACK_INDEX]: { currentTrackIndex: number };
+  [AUDIO_PLAYER_ACTIONS.SET_CURRENT_TIME]: { currentTime: number };
+  [AUDIO_PLAYER_ACTIONS.SET_DURATION]: { duration: number };
+  [AUDIO_PLAYER_ACTIONS.SET_IS_PLAYING]: { isPlaying: boolean | 'toggle' };
+};
+
+type ReducerAction = {
+  [K in keyof ActionPayloads]: ActionPayloads[K] extends undefined
+    ? { type: K }
+    : { type: K; payload: ActionPayloads[K] };
+}[keyof ActionPayloads];
+
+type State = {
+  currentTrackIndex: number;
+  currentTime: number;
+  duration: number;
+  isPlaying: boolean;
+};
+
+function getInitialState(defaultTrackIndex: number): State {
+  return {
+    currentTrackIndex: defaultTrackIndex,
+    currentTime: 0,
+    duration: 0,
+    isPlaying: false,
+  };
+}
+
+function audioPlayerReducer(state: State, action: ReducerAction) {
+  switch (action.type) {
+    case AUDIO_PLAYER_ACTIONS.SET_CURRENT_TIME:
+      return { ...state, ...action.payload };
+    case AUDIO_PLAYER_ACTIONS.SET_CURRENT_TRACK_INDEX:
+      return { ...state, ...action.payload };
+    case AUDIO_PLAYER_ACTIONS.SET_DURATION:
+      return { ...state, ...action.payload };
+    case AUDIO_PLAYER_ACTIONS.SET_IS_PLAYING:
+      return {
+        ...state,
+        isPlaying:
+          action.payload.isPlaying === 'toggle' ? !state.isPlaying : action.payload.isPlaying,
+      };
+    default:
+      return state;
+  }
+}
 
 export interface AudioTrackData {
   title: string;
@@ -16,21 +70,25 @@ export interface AudioTrackData {
   thumbnail?: string;
 }
 
-export interface AudioPlayerContextType {
+export interface AudioPlayerContextStateType extends State {
   audioRef: RefObject<HTMLAudioElement>;
-  currentTime: number;
   currentTrack: AudioTrackData | undefined;
-  duration: number;
-  isPlaying: boolean;
   progressBarRef: RefObject<HTMLInputElement>;
-  setCurrentTime: Dispatch<SetStateAction<number>>;
-  setCurrentTrackIndex: Dispatch<SetStateAction<number>>;
-  setDuration: Dispatch<SetStateAction<number>>;
-  setIsPlaying: Dispatch<SetStateAction<boolean>>;
   tracks: AudioTrackData[];
 }
 
-export const AudioPlayerContext = createContext<AudioPlayerContextType | undefined>(undefined);
+export interface AudioPlayerContextDispatchType {
+  dispatch: Dispatch<ReducerAction>;
+  actions: typeof AUDIO_PLAYER_ACTIONS;
+}
+
+export const AudioPlayerContextState = createContext<AudioPlayerContextStateType | undefined>(
+  undefined,
+);
+
+export const AudioPlayerContextDispatch = createContext<AudioPlayerContextDispatchType | undefined>(
+  undefined,
+);
 
 export interface AudioPlayerContextProviderProps {
   /** @default 0 */
@@ -42,30 +100,42 @@ export interface AudioPlayerContextProviderProps {
 export function AudioPlayerContextProvider(props: AudioPlayerContextProviderProps) {
   const { children, defaultTrackIndex = 0, tracks = [] } = props;
 
-  const [currentTrackIndex, setCurrentTrackIndex] = useState<number>(defaultTrackIndex);
-  const [currentTime, setCurrentTime] = useState<number>(0);
-  const [duration, setDuration] = useState<number>(0);
-  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [{ currentTime, duration, currentTrackIndex, isPlaying }, dispatch] = useReducer(
+    audioPlayerReducer,
+    null,
+    () => getInitialState(defaultTrackIndex),
+  );
 
   const audioRef = useRef<HTMLAudioElement>(null);
   const progressBarRef = useRef<HTMLInputElement>(null);
 
-  const contextValue = useMemo(
+  const stateContextValue: AudioPlayerContextStateType = useMemo(
     () => ({
       audioRef,
       progressBarRef,
+      currentTrackIndex,
       currentTime,
       duration,
       currentTrack: tracks[currentTrackIndex],
       isPlaying,
-      setCurrentTrackIndex,
-      setCurrentTime,
-      setDuration,
-      setIsPlaying,
       tracks,
     }),
     [currentTrackIndex, currentTime, duration, tracks, isPlaying],
   );
 
-  return <AudioPlayerContext.Provider value={contextValue}>{children}</AudioPlayerContext.Provider>;
+  const dispatchContextValue: AudioPlayerContextDispatchType = useMemo(
+    () => ({
+      dispatch,
+      actions: AUDIO_PLAYER_ACTIONS,
+    }),
+    [],
+  );
+
+  return (
+    <AudioPlayerContextDispatch.Provider value={dispatchContextValue}>
+      <AudioPlayerContextState.Provider value={stateContextValue}>
+        {children}
+      </AudioPlayerContextState.Provider>
+    </AudioPlayerContextDispatch.Provider>
+  );
 }
