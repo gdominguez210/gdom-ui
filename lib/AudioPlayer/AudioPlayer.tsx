ff28c4676bf55edb6204b7fd2f470b5d051fae9b
gdominguez210 <gdominguez210@live.com>
2025-03-02 01:13:53 -0500

    refactor(AudioPlayer): accept ref, add base component, add component that uses context

--- lib/AudioPlayer/AudioPlayer.tsx
+++ lib/AudioPlayer/AudioPlayer.tsx
@@ -1,6 +1,7 @@
-import clsx from 'clsx';
-import { type ComponentPropsWithoutRef, type ElementType } from 'react';
+import { type ComponentPropsWithoutRef, type ElementType, forwardRef, type Ref } from 'react';
 import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+import { useAudioPlayerContextState } from '@lib/AudioPlayerContextProvider';
 
 import { AudioPlayerAuthor } from '@lib/AudioPlayerAuthor/AudioPlayerAuthor';
 import { AudioPlayerContextProvider } from '@lib/AudioPlayerContextProvider/AudioPlayerContextProvider';
@@ -12,22 +13,43 @@ import { AudioPlayerTime } from '@lib/AudioPlayerTime/AudioPlayerTime';
 import { AudioPlayerTitle } from '@lib/AudioPlayerTitle/AudioPlayerTitle';
 import { AudioPlayerVolume } from '@lib/AudioPlayerVolume/AudioPlayerVolume';
 
-export type AudioPlayerProps<T extends ElementType = 'div'> = {
+export type AudioPlayerBaseProps<T extends ElementType = 'div'> = {
   /** @default div */
   as?: T;
 } & ComponentPropsWithoutRef<T>;
 
-export function AudioPlayer<T extends ElementType>(props: AudioPlayerProps<T>) {
-  const { as: Element = 'div', children, className } = props;
+function _AudioPlayerBase<T extends ElementType>(
+  props: AudioPlayerBaseProps<T>,
+  ref: Ref<T extends ElementType<infer E> ? E : HTMLElement>,
+) {
+  const { as: Element = 'div', children, className, ...restProps } = props;
 
   return (
     <Element
+      ref={ref}
       className={twMerge(
         clsx('flex flex-col justify-center bg-slate-700 text-neutral-100', className),
       )}
+      tabIndex={-1}
+      {...restProps}
     >
       {children}
     </Element>
+  );
+}
+
+export const AudioPlayerBase = forwardRef(_AudioPlayerBase);
+
+export type AudioPlayerProps<T extends ElementType = 'div'> = AudioPlayerBaseProps<T>;
+
+export function AudioPlayer<T extends ElementType>(props: AudioPlayerProps<T>) {
+  const { containerRef } = useAudioPlayerContextState();
+
+  return (
+    <AudioPlayerBase
+      {...props}
+      ref={containerRef}
+    />
   );
 }
 
