592f7547e11caebbd0f7c8e4695e5ce38057d722
gdominguez210 <gdominguez210@live.com>
2025-03-07 22:21:17 -0500

    refactor(AudioPlayerContextProvider/data): move AUDIO_PLAYER_ACTIONS, add AUDIO_PLAYER_CONTEXT_ERROR

--- lib/AudioPlayerContextProvider/data.tsx
+++ lib/AudioPlayerContextProvider/data.tsx
@@ -1,10 +1,16 @@
-import type { ReactNode } from 'react';
-import { trackData } from '@lib/AudioPlayer/data';
-import { AudioPlayerContextProvider } from '@lib/AudioPlayerContextProvider/AudioPlayerContextProvider';
+export const AUDIO_PLAYER_ACTIONS = {
+  SET_CURRENT_TRACK_INDEX: 'SET_CURRENT_TRACK_INDEX',
+  SET_CURRENT_TIME: 'SET_CURRENT_TIME',
+  SET_DURATION: 'SET_DURATION',
+  SET_IS_PLAYING: 'SET_IS_PLAYING',
+  SET_VOLUME: 'SET_VOLUME',
+  SET_MUTE: 'SET_MUTE',
+} as const;
 
-export const AUDIO_PLAYER_CONTEXT_ERROR =
-  'useAudioPlayerContext must be used within an AudioPlayerContextProvider';
+export const AUDIO_PLAYER_CONTEXT_ERROR = {
+  STATE: 'useAudioPlayerContextState must be used within an AudioPlayerContextProvider',
+  DISPATCH: 'useAudioPlayerContextDispatch must be used within an AudioPlayerContextProvider',
+} as const;
 
-export const AudioPlayerContextProviderWithTrackData = ({ children }: { children: ReactNode }) => (
-  <AudioPlayerContextProvider tracks={trackData}>{children}</AudioPlayerContextProvider>
-);
+export type AudioPlayerContextErrorType =
+  (typeof AUDIO_PLAYER_CONTEXT_ERROR)[keyof typeof AUDIO_PLAYER_CONTEXT_ERROR];
