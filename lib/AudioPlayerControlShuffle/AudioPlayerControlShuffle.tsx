6066c7dd585b1efa84eb728d794bf4fbfed47e97
gdominguez210 <gdominguez210@live.com>
2025-03-14 21:39:52 -0400

    feat(lib/AudioPlayerControlShuffle): add AudioPlayerControlShuffle

---
+++ lib/AudioPlayerControlShuffle/AudioPlayerControlShuffle.tsx
@@ -0,0 +1,52 @@
+import { type ComponentPropsWithRef, type MouseEventHandler, useCallback } from 'react';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+import { Icon } from '@lib/Icon';
+import { useAudioPlayerContextAudio } from '@lib/AudioPlayerContextAudioProvider';
+
+export interface AudioPlayerControlShufflePrimitiveProps extends ComponentPropsWithRef<'button'> {
+  active?: boolean;
+}
+
+export function AudioPlayerControlShufflePrimitive(props: AudioPlayerControlShufflePrimitiveProps) {
+  const { active = false, className, ...restProps } = props;
+
+  return (
+    <button
+      className={twMerge(
+        clsx({ 'text-neutral-100/50': !active }, 'hover:text-neutral-100', className),
+      )}
+      aria-label="Toggle Shuffle"
+      aria-pressed={active}
+      {...restProps}
+    >
+      <Icon
+        name="shuffle-fill"
+        className="scale-75"
+      />
+    </button>
+  );
+}
+
+export function AudioPlayerControlShuffle(
+  props: Omit<AudioPlayerControlShufflePrimitiveProps, 'active'>,
+) {
+  const { onClick, ...restProps } = props;
+  const { shuffle, toggleShuffle } = useAudioPlayerContextAudio();
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+    (e) => {
+      toggleShuffle();
+      onClick?.(e);
+    },
+    [toggleShuffle, onClick],
+  );
+
+  return (
+    <AudioPlayerControlShufflePrimitive
+      active={shuffle}
+      onClick={handleClick}
+      {...restProps}
+    />
+  );
+}
