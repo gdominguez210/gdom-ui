16bd92c4f07b521f04fcd50c3d61fbf8b3144ff8
gdominguez210 <gdominguez210@live.com>
2025-03-15 22:38:01 -0400

    test(lib/AudioPlayerContextTrackProvider): add unit test

--- lib/AudioPlayerContextTrackProvider/AudioPlayerContextTrackProvider.tsx
+++ lib/AudioPlayerContextTrackProvider/AudioPlayerContextTrackProvider.tsx
@@ -1,47 +1,6 @@
 import { type PropsWithChildren, useReducer, useMemo, useCallback } from 'react';
 import { AudioPlayerContextTrack } from './AudioPlayerContextTrack';
-
-export type AudioTrackData = {
-  title: string;
-  src: string;
-  author: string;
-  thumbnail?: string;
-};
-
-export type TrackProviderState = {
-  currentTrackIndex: number;
-  tracks: AudioTrackData[];
-  currentTrack: AudioTrackData | undefined;
-};
-
-export type TrackProviderActions = {
-  setTrackIndex: (index: number) => void;
-};
-
-const TRACK_ACTIONS = {
-  SET_CURRENT_TRACK_INDEX: 'SET_CURRENT_TRACK_INDEX',
-} as const;
-
-type ActionPayloads = {
-  [TRACK_ACTIONS.SET_CURRENT_TRACK_INDEX]: { currentTrackIndex: number };
-};
-
-type TrackProviderAction = {
-  [K in keyof ActionPayloads]: { type: K; payload: ActionPayloads[K] };
-}[keyof ActionPayloads];
-
-function trackReducer(state: TrackProviderState, action: TrackProviderAction): TrackProviderState {
-  switch (action.type) {
-    case TRACK_ACTIONS.SET_CURRENT_TRACK_INDEX:
-      return {
-        ...state,
-        currentTrackIndex: action.payload.currentTrackIndex,
-        currentTrack: state.tracks[action.payload.currentTrackIndex],
-      };
-    default:
-      return state;
-  }
-}
+import { trackReducer, TRACK_ACTIONS, type AudioTrackData } from './reducer';
 
 export interface AudioPlayerContextTrackProviderProps extends PropsWithChildren {
   tracks: AudioTrackData[];
