2a8e0feeb433d5484247d6633b1b29f03ed95e50
gdominguez210 <gdominguez210@live.com>
2025-03-17 23:37:03 -0400

    refactor(lib/AudioPlayerContextTrackProvider): memoize component

--- lib/AudioPlayerContextTrackProvider/AudioPlayerContextTrackProvider.tsx
+++ lib/AudioPlayerContextTrackProvider/AudioPlayerContextTrackProvider.tsx
@@ -1,4 +1,4 @@
-import { type PropsWithChildren, useReducer, useMemo, useCallback } from 'react';
+import { type PropsWithChildren, useReducer, useMemo, useCallback, memo } from 'react';
 import { AudioPlayerContextTrack } from './AudioPlayerContextTrack';
 import { trackReducer, TRACK_ACTIONS, type AudioTrackData } from './reducer';
 
@@ -7,7 +7,7 @@ export interface AudioPlayerContextTrackProviderProps extends PropsWithChildren 
   defaultTrackIndex?: number;
 }
 
-export function AudioPlayerContextTrackProvider(props: AudioPlayerContextTrackProviderProps) {
+function AudioPlayerContextTrackProvider(props: AudioPlayerContextTrackProviderProps) {
   const { children, tracks, defaultTrackIndex = 0 } = props;
 
   const [state, dispatch] = useReducer(trackReducer, {
@@ -37,3 +37,7 @@ export function AudioPlayerContextTrackProvider(props: AudioPlayerContextTrackPr
     </AudioPlayerContextTrack.Provider>
   );
 }
+
+const AudioPlayerContextTrackProviderMemo = memo(AudioPlayerContextTrackProvider);
+AudioPlayerContextTrackProviderMemo.displayName = 'AudioPlayerContextTrackProvider';
+export { AudioPlayerContextTrackProviderMemo as AudioPlayerContextTrackProvider };
