e439108514378ed3017e6f65ff28faaaefa9c0c7
gdominguez210 <gdominguez210@live.com>
2025-03-17 23:37:41 -0400

    refactor(lib/AudioPlayer): memoize component

--- lib/AudioPlayer/AudioPlayer.tsx
+++ lib/AudioPlayer/AudioPlayer.tsx
@@ -1,4 +1,4 @@
-import { type ComponentPropsWithRef, type ElementType } from 'react';
+import { type ComponentPropsWithRef, type ElementType, memo } from 'react';
 import { twMerge } from 'tailwind-merge';
 import clsx from 'clsx';
 import { AudioPlayerAuthor } from '@lib/AudioPlayerAuthor/AudioPlayerAuthor';
@@ -17,7 +17,7 @@ export type AudioPlayerPrimitiveProps<T extends ElementType = 'div'> = {
   as?: T;
 } & ComponentPropsWithRef<T>;
 
-export function AudioPlayerPrimitive<T extends ElementType>(props: AudioPlayerPrimitiveProps<T>) {
+function AudioPlayerPrimitive<T extends ElementType>(props: AudioPlayerPrimitiveProps<T>) {
   const { as: Element = 'div', children, className, ...restProps } = props;
 
   return (
@@ -33,6 +33,10 @@ export function AudioPlayerPrimitive<T extends ElementType>(props: AudioPlayerPr
   );
 }
 
+const AudioPlayerPrimitiveMemo = memo(AudioPlayerPrimitive);
+AudioPlayerPrimitiveMemo.displayName = 'AudioPlayerPrimitive';
+export { AudioPlayerPrimitiveMemo as AudioPlayerPrimitive };
+
 export type AudioPlayerProps<T extends ElementType = 'div'> = AudioPlayerPrimitiveProps<T> & {
   tracks: AudioTrackData[];
   /** @default 0 */
@@ -41,7 +45,7 @@ export type AudioPlayerProps<T extends ElementType = 'div'> = AudioPlayerPrimiti
   defaultVolume?: number;
 };
 
-export function AudioPlayer<T extends ElementType = 'div'>(props: AudioPlayerProps<T>) {
+function AudioPlayer<T extends ElementType = 'div'>(props: AudioPlayerProps<T>) {
   const { tracks, defaultTrackIndex = 0, defaultVolume = 50, ...restProps } = props;
 
   return (
@@ -55,11 +59,27 @@ export function AudioPlayer<T extends ElementType = 'div'>(props: AudioPlayerPro
   );
 }
 
-AudioPlayer.Author = AudioPlayerAuthor;
-AudioPlayer.Controls = AudioPlayerControls;
-AudioPlayer.Image = AudioPlayerImage;
-AudioPlayer.Info = AudioPlayerInfo;
-AudioPlayer.ProgressBar = AudioPlayerProgressBar;
-AudioPlayer.Time = AudioPlayerTime;
-AudioPlayer.Title = AudioPlayerTitle;
-AudioPlayer.Volume = AudioPlayerVolume;
+const AudioPlayerRoot = memo(AudioPlayer);
+AudioPlayerRoot.displayName = 'AudioPlayer';
+
+const AudioPlayerComponent = Object.assign(AudioPlayerRoot, {
+  Author: AudioPlayerAuthor,
+  Controls: AudioPlayerControls,
+  Image: AudioPlayerImage,
+  Info: AudioPlayerInfo,
+  ProgressBar: AudioPlayerProgressBar,
+  Time: AudioPlayerTime,
+  Title: AudioPlayerTitle,
+  Volume: AudioPlayerVolume,
+}) as typeof AudioPlayerRoot & {
+  Author: typeof AudioPlayerAuthor;
+  Controls: typeof AudioPlayerControls;
+  Image: typeof AudioPlayerImage;
+  Info: typeof AudioPlayerInfo;
+  ProgressBar: typeof AudioPlayerProgressBar;
+  Time: typeof AudioPlayerTime;
+  Title: typeof AudioPlayerTitle;
+  Volume: typeof AudioPlayerVolume;
+};
+
+export { AudioPlayerComponent as AudioPlayer };
