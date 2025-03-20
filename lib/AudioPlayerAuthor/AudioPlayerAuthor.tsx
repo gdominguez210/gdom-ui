3a36eb1659a81c1b463db567c97ea4946530802a
gdominguez210 <gdominguez210@live.com>
2025-03-19 23:21:51 -0400

    refactor(lib/AudioPlayer): remove memoization, add JSDoc comments

--- lib/AudioPlayerAuthor/AudioPlayerAuthor.tsx
+++ lib/AudioPlayerAuthor/AudioPlayerAuthor.tsx
@@ -1,14 +1,22 @@
-import { type ComponentPropsWithRef, type ElementType, memo } from 'react';
+import { type ComponentPropsWithRef, type ElementType } from 'react';
 import { twMerge } from 'tailwind-merge';
 import clsx from 'clsx';
 import { useAudioPlayerContextTrack } from '@lib/AudioPlayerContextTrackProvider/useAudioPlayerContextTrack';
 
+/**
+ * Props for the track author component
+ */
 export type AudioPlayerAuthorProps<T extends ElementType = 'p'> = {
-  /** @default p */
+  /** Element to render as @default p */
   as?: T;
 } & ComponentPropsWithRef<T>;
 
-function AudioPlayerAuthorPrimitive<T extends ElementType>(props: AudioPlayerAuthorProps<T>) {
+/**
+ * Base component for displaying author information with appropriate styling
+ */
+export function AudioPlayerAuthorPrimitive<T extends ElementType>(
+  props: AudioPlayerAuthorProps<T>,
+) {
   const { as: Element = 'p', children, className, ...restProps } = props;
 
   return (
@@ -21,11 +29,11 @@ function AudioPlayerAuthorPrimitive<T extends ElementType>(props: AudioPlayerAut
   );
 }
 
-const AudioPlayerAuthorPrimitiveMemo = memo(AudioPlayerAuthorPrimitive);
-AudioPlayerAuthorPrimitiveMemo.displayName = 'AudioPlayerAuthorPrimitive';
-export { AudioPlayerAuthorPrimitiveMemo as AudioPlayerAuthorPrimitive };
-
-function AudioPlayerAuthor<T extends ElementType>(props: AudioPlayerAuthorProps<T>) {
+/**
+ * Displays the author of the current audio track
+ * Returns null if no author is available
+ */
+export function AudioPlayerAuthor<T extends ElementType = 'p'>(props: AudioPlayerAuthorProps<T>) {
   const { currentTrack: { author } = {} } = useAudioPlayerContextTrack();
 
   if (!author) return null;
@@ -39,7 +47,3 @@ function AudioPlayerAuthor<T extends ElementType>(props: AudioPlayerAuthorProps<
     </AudioPlayerAuthorPrimitive>
   );
 }
-
-const AudioPlayerAuthorMemo = memo(AudioPlayerAuthor);
-AudioPlayerAuthorMemo.displayName = 'AudioPlayerAuthor';
-export { AudioPlayerAuthorMemo as AudioPlayerAuthor };
