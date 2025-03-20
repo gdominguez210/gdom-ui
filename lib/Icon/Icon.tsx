e637ceca459e3a073bcf9fb2e8bfe52f4ec74d54
gdominguez210 <gdominguez210@live.com>
2025-03-20 14:07:41 -0400

    refactor(lib/Icon): update to React 19 syntax

--- lib/Icon/Icon.tsx
+++ lib/Icon/Icon.tsx
@@ -1,13 +1,13 @@
 import { type IconName, icons } from './data';
 import clsx from 'clsx';
-import { type SVGAttributes, type Ref, forwardRef } from 'react';
+import { type ComponentPropsWithRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export interface IconProps extends SVGAttributes<SVGElement> {
+export interface IconProps extends ComponentPropsWithRef<'svg'> {
   name: IconName;
 }
 
-function _Icon(props: IconProps, ref: Ref<SVGSVGElement>) {
+export function Icon(props: IconProps) {
   const { name, className, ...restProps } = props;
 
   const Icon = icons[name];
@@ -16,9 +16,6 @@ function _Icon(props: IconProps, ref: Ref<SVGSVGElement>) {
     <Icon
       className={twMerge(clsx('h-[1em] fill-current', className))}
       {...restProps}
-      ref={ref}
     />
   );
 }
-
-export const Icon = forwardRef<SVGSVGElement, IconProps>(_Icon);
