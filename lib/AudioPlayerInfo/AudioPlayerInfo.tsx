5886e4e0cd22525dd2d15fd59af52b0fe6934e89
gdominguez210 <gdominguez210@live.com>
2025-03-19 23:36:17 -0400

    refactor(lib/AudioPlayerInfo): remove memoization, add JSDoc comments

--- lib/AudioPlayerInfo/AudioPlayerInfo.tsx
+++ lib/AudioPlayerInfo/AudioPlayerInfo.tsx
@@ -1,13 +1,19 @@
-import { type ComponentPropsWithRef, type ElementType, memo } from 'react';
+import { type ComponentPropsWithRef, type ElementType } from 'react';
 import { twMerge } from 'tailwind-merge';
 import clsx from 'clsx';
 
+/**
+ * Props for the track information container component
+ */
 export type AudioPlayerInfoProps<T extends ElementType = 'div'> = {
-  /** @default div */
+  /** Element to render as @default div */
   as?: T;
 } & ComponentPropsWithRef<T>;
 
-function AudioPlayerInfo<T extends ElementType>(props: AudioPlayerInfoProps<T>) {
+/**
+ * Container component for displaying track information (image, title, artist)
+ */
+export function AudioPlayerInfo<T extends ElementType>(props: AudioPlayerInfoProps<T>) {
   const { as: Element = 'div', className, children, ...restProps } = props;
 
   return (
@@ -19,7 +25,3 @@ function AudioPlayerInfo<T extends ElementType>(props: AudioPlayerInfoProps<T>) 
     </Element>
   );
 }
-
-const AudioPlayerInfoMemo = memo(AudioPlayerInfo);
-AudioPlayerInfoMemo.displayName = 'AudioPlayerInfo';
-export { AudioPlayerInfoMemo as AudioPlayerInfo };
