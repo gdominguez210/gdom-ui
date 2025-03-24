e6efac052108c8fda46bf2c06e7f0a2c2f37f714
gdominguez210 <gdominguez210@live.com>
2025-03-24 19:01:02 -0400

    feat(lib/AudioPlayerControlButton): add active prop support

--- lib/AudioPlayerControlButton/AudioPlayerControlButton.tsx
+++ lib/AudioPlayerControlButton/AudioPlayerControlButton.tsx
@@ -2,16 +2,19 @@ import { clsx } from 'clsx';
 import { type ComponentPropsWithRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-type AudioPlayerControlButtonProps = ComponentPropsWithRef<'button'>;
+type AudioPlayerControlButtonProps = ComponentPropsWithRef<'button'> & {
+  active?: boolean;
+};
 
 export function AudioPlayerControlButton(props: AudioPlayerControlButtonProps) {
-  const { className, children, ...restProps } = props;
+  const { active = false, className, children, ...restProps } = props;
   return (
     <button
       className={twMerge(
         clsx(
           'rounded-md p-2 hover:bg-black/30',
-          'focus-within:outline-1 focus-within:outline-white focus-visible:bg-black/30',
+          'focus-within:outline-1 focus-within:outline-white focus:bg-black/30',
+          active && 'bg-black/30',
           className,
         ),
       )}
