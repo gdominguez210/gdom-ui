eb10fe800e7d22d06c25f040f1b974b97abfe4cd
gdominguez210 <gdominguez210@live.com>
2025-03-24 19:01:24 -0400

    chore(lib/AudioPlayer/data): add new tracks

--- lib/AudioPlayer/data.ts
+++ lib/AudioPlayer/data.ts
@@ -2,39 +2,63 @@ import type { AudioTrackData } from '@lib/AudioPlayerContextTrackProvider/reduce
 
 export const trackData: AudioTrackData[] = [
   {
+    title: 'Polyphia Type Beat',
+    src: 'https://cdn1.suno.ai/58730401-c910-4a77-935e-83d71d5d1a52.mp3',
+    author: 'Gary Dominguez',
+    thumbnail: 'https://cdn2.suno.ai/image_58730401-c910-4a77-935e-83d71d5d1a52.jpeg',
+  },
+  {
+    title: 'Polyphia Type Beat #2',
+    src: 'https://cdn1.suno.ai/cc26e6a5-ebda-4d56-8bad-8fa27d7a8a1f.mp3',
+    author: 'Gary Dominguez',
+    thumbnail: 'https://cdn2.suno.ai/image_703e2698-aa6d-4f1c-8c32-2cebd620f73d.jpeg',
+  },
+  {
+    title: 'Untitled',
+    src: 'https://cdn1.suno.ai/c5b2457d-edd2-42d7-9079-c73c51df2f17.mp3',
+    author: 'Gary Dominguez',
+    thumbnail: 'https://cdn2.suno.ai/image_8aee68b6-6066-41d7-b901-5af9b9f912c6.jpeg',
+  },
+  {
+    title: 'Untitled #2',
+    src: 'https://cdn1.suno.ai/2ba503d0-384e-44cb-83b4-cc15e40e6d73.mp3',
+    author: 'Gary Dominguez',
+    thumbnail: 'https://cdn2.suno.ai/image_2ba503d0-384e-44cb-83b4-cc15e40e6d73.jpeg',
+  },
+  {
     title: 'Electro Swing House',
     src: 'https://cdn1.suno.ai/2fdedd0d-a05b-4dda-8c8d-09ba8697a50f.mp3',
     author: 'Gary Dominguez',
     thumbnail: 'https://cdn2.suno.ai/image_7fa89cca-b468-4ec4-9649-70dd745acd12.jpeg',
   },
   {
-    title: 'K-Block (Demo)',
+    title: 'K-Block',
     src: 'https://cdn1.suno.ai/a9ef09b8-fbc1-4bd4-ac0c-4a9e82c561ca.mp3',
     author: 'Gary Dominguez',
     thumbnail: 'https://cdn2.suno.ai/image_a9ef09b8-fbc1-4bd4-ac0c-4a9e82c561ca.jpeg',
   },
-  {
-    title: 'Doomcore',
-    src: 'https://cdn1.suno.ai/e5130783-0f8b-4496-8fcd-ae58c0fa02f2.mp3',
-    author: 'Gary Dominguez',
-    thumbnail: 'https://cdn2.suno.ai/image_ca4a5344-69d6-4967-87a5-324825d0ebaf.jpeg',
-  },
-  {
-    title: 'Doomcore V2',
-    src: 'https://cdn1.suno.ai/0ec8d8ef-3f08-4d1a-a7d4-b51536e9b1a7.mp3',
-    author: 'Gary Dominguez',
-    thumbnail: 'https://cdn2.suno.ai/image_6b1d56d3-c2aa-4b5d-b759-ff9fa2055746.jpeg',
-  },
-  {
-    title: 'Adrift in Space',
-    src: 'https://cdn1.suno.ai/f4e9bdb8-04c6-4e98-9ea2-8b4bce21156e.mp3',
-    author: 'Gary Dominguez',
-    thumbnail: 'https://cdn2.suno.ai/image_7a168d5e-ebbe-470a-a5cd-aebe3318204c.jpeg',
-  },
-  {
-    title: 'The Awakening',
-    src: 'https://cdn1.suno.ai/582c6984-bbfd-44cb-a2df-6be4e4cdadf8.mp3',
-    author: 'Gary Dominguez',
-    thumbnail: 'https://cdn2.suno.ai/image_0769ed7e-5990-4b23-986a-2ca3281e1ac6.jpeg',
-  },
+  // {
+  //   title: 'Doomcore',
+  //   src: 'https://cdn1.suno.ai/e5130783-0f8b-4496-8fcd-ae58c0fa02f2.mp3',
+  //   author: 'Gary Dominguez',
+  //   thumbnail: 'https://cdn2.suno.ai/image_ca4a5344-69d6-4967-87a5-324825d0ebaf.jpeg',
+  // },
+  // {
+  //   title: 'Doomcore V2',
+  //   src: 'https://cdn1.suno.ai/0ec8d8ef-3f08-4d1a-a7d4-b51536e9b1a7.mp3',
+  //   author: 'Gary Dominguez',
+  //   thumbnail: 'https://cdn2.suno.ai/image_6b1d56d3-c2aa-4b5d-b759-ff9fa2055746.jpeg',
+  // },
+  // {
+  //   title: 'Adrift in Space',
+  //   src: 'https://cdn1.suno.ai/f4e9bdb8-04c6-4e98-9ea2-8b4bce21156e.mp3',
+  //   author: 'Gary Dominguez',
+  //   thumbnail: 'https://cdn2.suno.ai/image_7a168d5e-ebbe-470a-a5cd-aebe3318204c.jpeg',
+  // },
+  // {
+  //   title: 'The Awakening',
+  //   src: 'https://cdn1.suno.ai/582c6984-bbfd-44cb-a2df-6be4e4cdadf8.mp3',
+  //   author: 'Gary Dominguez',
+  //   thumbnail: 'https://cdn2.suno.ai/image_0769ed7e-5990-4b23-986a-2ca3281e1ac6.jpeg',
+  // },
 ] as const;
