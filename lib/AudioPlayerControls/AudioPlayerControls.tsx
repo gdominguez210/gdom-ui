a8f0d2ddd5fdb08f486f2ec1aef2f0fe7137a076
gdominguez210 <gdominguez210@live.com>
2025-03-23 23:33:32 -0400

    style(AudioPlayerControls): use gap-1

--- lib/AudioPlayerControls/AudioPlayerControls.tsx
+++ lib/AudioPlayerControls/AudioPlayerControls.tsx
@@ -26,7 +26,7 @@ export function AudioPlayerControlsPrimitive<T extends ElementType>(
 
   return (
     <Element
-      className={twMerge(clsx('flex items-center justify-center gap-4 p-4 text-2xl', className))}
+      className={twMerge(clsx('flex items-center justify-center gap-1 p-4 text-2xl', className))}
       {...restProps}
     >
       {children}
