2b7e1415d5f8e0ac646e28e2accdb666969a93c8
gdominguez210 <gdominguez210@live.com>
2025-03-25 21:01:24 -0400

    feat(lib/AudioPlaylistTracks): add AudioPlaylistTracks component

---
+++ lib/AudioPlaylistTracks/AudioPlaylistTracks.tsx
@@ -0,0 +1,29 @@
+import { type ComponentPropsWithRef, type ElementType } from 'react';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+
+/**
+ * Props for the audio playlist tracks component
+ */
+export type AudioPlaylistTracksProps<T extends ElementType = 'ul'> = {
+  /** Element to render as @default ul */
+  as?: T;
+} & ComponentPropsWithRef<T>;
+
+/**
+ * Component for rendering the scrollable list of playlist tracks
+ */
+export function AudioPlaylistTracks<T extends ElementType = 'ul'>(
+  props: AudioPlaylistTracksProps<T>,
+) {
+  const { as: Element = 'ul', className, children, ...restProps } = props;
+
+  return (
+    <Element
+      className={twMerge(clsx('flex flex-col gap-2 p-4', className))}
+      {...restProps}
+    >
+      {children}
+    </Element>
+  );
+}
