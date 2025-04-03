e45d56b7d40b55e345cadd49671fd7f02205cf9f
gdominguez210 <gdominguez210@live.com>
2025-04-03 13:41:27 -0400

    refactor(lib/AudioPlaylist/namespace): update namespace to reflect new AudioPlaylistTrack architecture

--- lib/AudioPlaylist/namespace.ts
+++ lib/AudioPlaylist/namespace.ts
@@ -1,15 +1,13 @@
 import { AudioPlaylistContextProvider } from '@lib/AudioPlaylistContextProvider';
 import { AudioPlaylist } from './AudioPlaylist';
-import { AudioPlaylistTrackTitle } from '@lib/AudioPlaylistTrackTitle/AudioPlaylistTrackTitle';
-import { AudioPlaylistTrack } from '@lib/AudioPlaylistTrack/AudioPlaylistTrack';
-import { AudioPlaylistTrackAuthor } from '@lib/AudioPlaylistTrackAuthor/AudioPlaylistTrackAuthor';
-import { AudioPlaylistTrackImage } from '@lib/AudioPlaylistTrackImage/AudioPlaylistTrackImage';
 import { AudioPlaylistTracks } from '@lib/AudioPlaylistTracks/AudioPlaylistTracks';
 import { AudioPlaylistHeader } from '@lib/AudioPlaylistHeader/AudioPlaylistHeader';
 import { AudioPlaylistDismiss } from '@lib/AudioPlaylistDismiss/AudioPlaylistDismiss';
 import { AudioPlaylistControlToggle } from '@lib/AudioPlaylistControlToggle/AudioPlaylistControlToggle';
 import { AudioPlaylistExpandableContainer } from '@lib/AudioPlaylistExpandableContainer';
 import { AudioPlaylistScrollableContainer } from '@lib/AudioPlaylistScrollableContainer';
+import { AudioPlaylistTrackCompoundComponent } from '@lib/AudioPlaylistTrack/namespace';
+
 export const AudioPlaylistCompoundComponent = {
   Root: Object.assign(AudioPlaylist, { displayName: 'AudioPlaylist.Root' }),
   Provider: Object.assign(AudioPlaylistContextProvider, { displayName: 'AudioPlaylist.Provider' }),
@@ -19,12 +17,7 @@ export const AudioPlaylistCompoundComponent = {
   ControlToggle: Object.assign(AudioPlaylistControlToggle, {
     displayName: 'AudioPlaylist.ControlToggle',
   }),
-  Track: Object.assign(AudioPlaylistTrack, { displayName: 'AudioPlaylist.Track' }),
-  TrackTitle: Object.assign(AudioPlaylistTrackTitle, { displayName: 'AudioPlaylist.TrackTitle' }),
-  TrackAuthor: Object.assign(AudioPlaylistTrackAuthor, {
-    displayName: 'AudioPlaylist.TrackAuthor',
-  }),
-  TrackImage: Object.assign(AudioPlaylistTrackImage, { displayName: 'AudioPlaylist.TrackImage' }),
+  Track: Object.assign(AudioPlaylistTrackCompoundComponent, { displayName: 'AudioPlaylist.Track' }),
   ExpandableContainer: Object.assign(AudioPlaylistExpandableContainer, {
     displayName: 'AudioPlaylist.ExpandableContainer',
   }),
