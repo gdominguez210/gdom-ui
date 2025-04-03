ab0169bc3d0ea1f79fef2f3c434ac60416acf5bb
gdominguez210 <gdominguez210@live.com>
2025-04-03 13:38:25 -0400

    refactor(lib/AudioPlaylistTrackImage): split into context and primitive component

--- lib/AudioPlaylistTrackImage/AudioPlaylistTrackImage.tsx
+++ lib/AudioPlaylistTrackImage/AudioPlaylistTrackImage.tsx
@@ -1,57 +1,24 @@
-import { AudioPlayerControlPlayPrimitive } from '@lib/AudioPlayerControlPlay/AudioPlayerControlPlayPrimitive';
-import {
-  AudioPlayerImagePrimitive,
-  type AudioPlayerImagePrimitiveProps,
-} from '@lib/AudioPlayerImage/AudioPlayerImagePrimitive';
-import clsx from 'clsx';
-import { type ElementType } from 'react';
+import type { ElementType } from 'react';
+import { useAudioPlaylistTrackContext } from '@lib/AudioPlaylistTrackContextProvider/useAudioPlaylistTrackContext';
+import { AudioPlaylistTrackImagePrimitive } from '@lib/AudioPlaylistTrackImage/AudioPlaylistTrackImagePrimitive';
+import { type AudioPlayerImageProps } from '@lib/AudioPlayerImage/AudioPlayerImage';
 
-/**
- * Props for the audio playlist track image component
- */
-export type AudioPlaylistTrackImageProps<T extends ElementType = 'div'> =
-  AudioPlayerImagePrimitiveProps<T> & {
-    active?: boolean;
-    isPlaying?: boolean;
-  };
+export type AudioPlaylistTrackImageProps<T extends ElementType = 'div'> = AudioPlayerImageProps<T>;
 
-/**
- * Component for displaying an audio playlist track image
- */
 export function AudioPlaylistTrackImage(props: AudioPlaylistTrackImageProps) {
   const {
-    src,
-    altText,
-    active = false,
-    isPlaying = false,
-    width = 48,
-    height = 48,
-    className,
-    ...restProps
-  } = props;
+    active,
+    isPlaying,
+    track: { thumbnail = '', title },
+  } = useAudioPlaylistTrackContext();
 
   return (
-    <div className="group relative h-12 w-12 shrink-0 overflow-hidden rounded-md">
-      <AudioPlayerImagePrimitive
-        src={src}
-        altText={altText}
-        width={width}
-        height={height}
-        className={clsx('h-full w-full rounded-md', { 'border-2 border-white': active }, className)}
-        {...restProps}
-      />
-      <div
-        className={clsx('absolute inset-0 flex items-center justify-center transition-opacity', {
-          'opacity-100': active,
-          'opacity-0 group-hover:opacity-100 group-focus:opacity-100': !active,
-        })}
-      >
-        <AudioPlayerControlPlayPrimitive
-          tabIndex={-1}
-          active={active && isPlaying}
-          className="border-none bg-transparent p-0 text-xl shadow-none hover:bg-transparent focus:bg-transparent focus:outline-hidden"
-        />
-      </div>
-    </div>
+    <AudioPlaylistTrackImagePrimitive
+      {...props}
+      active={active}
+      isPlaying={isPlaying}
+      src={thumbnail}
+      altText={`${title} thumbnail`}
+    />
   );
 }
--- lib/AudioPlaylistTrackImage/index.ts
+++ lib/AudioPlaylistTrackImage/index.ts
@@ -1,4 +1,9 @@
 export {
   AudioPlaylistTrackImage,
   type AudioPlaylistTrackImageProps,
-} from './AudioPlaylistTrackImage';
+} from '@lib/AudioPlaylistTrackImage/AudioPlaylistTrackImage';
+
+export {
+  AudioPlaylistTrackImagePrimitive,
+  type AudioPlaylistTrackImagePrimitiveProps,
+} from '@lib/AudioPlaylistTrackImage/AudioPlaylistTrackImagePrimitive';
