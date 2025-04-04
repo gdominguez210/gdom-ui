717fee8a8699018c452207e3fde88ebf9b9f26d8
gdominguez210 <gdominguez210@live.com>
2025-04-04 19:13:31 -0400

    refactor(lib/AudioPlayerControlButton): add type=“button”

--- lib/AudioPlayerControlButton/AudioPlayerControlButton.tsx
+++ lib/AudioPlayerControlButton/AudioPlayerControlButton.tsx
@@ -17,6 +17,7 @@ export function AudioPlayerControlButton(props: AudioPlayerControlButtonProps) {
           className,
         ),
       )}
+      type="button"
       {...restProps}
     >
       {children}
