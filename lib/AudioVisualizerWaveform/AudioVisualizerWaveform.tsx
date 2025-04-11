34487c5ace564340cbcda45541a5bedf9ee19103
gdominguez210 <gdominguez210@live.com>
2025-04-11 15:27:47 -0400

    refactor(lib/AudioVisualizerWaveform): update types

--- lib/AudioVisualizerWaveform/AudioVisualizerWaveform.tsx
+++ lib/AudioVisualizerWaveform/AudioVisualizerWaveform.tsx
@@ -5,11 +5,11 @@ import { twMerge } from 'tailwind-merge';
 import clsx from 'clsx';
 import {
   useAudioAnalyzer,
-  type AudioAnalyzerOptions,
+  type UseAudioAnalyzerOptions,
 } from '@lib/useAudioAnalyzer/useAudioAnalyzer';
 
 export type AudioVisualizerWaveformProps = ComponentPropsWithRef<'canvas'> &
-  Omit<AudioAnalyzerOptions, 'dataType' | 'onAnalyze'>;
+  Omit<UseAudioAnalyzerOptions, 'dataType' | 'onAnalyze'>;
 
 export function AudioVisualizerWaveform(props: AudioVisualizerWaveformProps) {
   const {
