a95de5224c661543e3a95abd929b9f630ed9b3c3
gdominguez210 <gdominguez210@live.com>
2025-04-11 15:22:11 -0400

    feat(lib/AudioVisualizerFrequencyBars): add AudioVisualizerFrequencyBars

---
+++ lib/AudioVisualizerFrequencyBars/AudioVisualizerFrequencyBars.tsx
@@ -0,0 +1,57 @@
+import { useAudioVisualizerFrequencyBars } from '@lib/AudioVisualizerFrequencyBars/useAudioVisualizerFrequencyBars';
+import { type ComponentPropsWithRef, type RefObject } from 'react';
+import { useComposedRefs } from '@lib/useComposedRefs/useComposedRefs';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
+import {
+  useAudioAnalyzer,
+  type AudioAnalyzerOptions,
+} from '@lib/useAudioAnalyzer/useAudioAnalyzer';
+
+export type AudioVisualizerFrequencyBarsProps = ComponentPropsWithRef<'canvas'> &
+  Omit<AudioAnalyzerOptions, 'dataType' | 'onAnalyze'>;
+
+export function AudioVisualizerFrequencyBars(props: AudioVisualizerFrequencyBarsProps) {
+  const {
+    className,
+    ref,
+    isPlaying,
+    audioRef,
+    duration,
+    audioContextRef,
+    isAudioContextReady,
+    fftSize,
+    smoothingTimeConstant,
+    frameRate,
+    createAudioSource,
+    deleteAudioSource,
+    ...restProps
+  } = props;
+
+  const { canvasRef, drawFrequencyBars } = useAudioVisualizerFrequencyBars();
+
+  const mergedRef = useComposedRefs(ref, canvasRef);
+
+  useAudioAnalyzer({
+    audioRef: audioRef as RefObject<HTMLAudioElement>,
+    audioContextRef,
+    isAudioContextReady,
+    isPlaying,
+    duration,
+    onAnalyze: drawFrequencyBars,
+    dataType: 'frequency',
+    fftSize,
+    smoothingTimeConstant,
+    frameRate,
+    createAudioSource,
+    deleteAudioSource,
+  });
+
+  return (
+    <canvas
+      className={twMerge(clsx('w-full max-w-full', className))}
+      ref={mergedRef}
+      {...restProps}
+    />
+  );
+}
