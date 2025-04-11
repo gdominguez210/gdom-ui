85122cd840cdc41825213825628448034958b0da
gdominguez210 <gdominguez210@live.com>
2025-04-11 15:26:11 -0400

    feat(lib/AudioPlayer): add visualizer subcomponents

--- lib/AudioPlayer/namespace.ts
+++ lib/AudioPlayer/namespace.ts
@@ -16,11 +16,17 @@ import { AudioPlayerControlNext } from '@lib/AudioPlayerControlNext/AudioPlayerC
 import { AudioPlayerControlShuffle } from '@lib/AudioPlayerControlShuffle/AudioPlayerControlShuffle';
 import { AudioPlayerControlLoop } from '@lib/AudioPlayerControlLoop/AudioPlayerControlLoop';
 import { AudioPlayer } from '@lib/AudioPlayer/AudioPlayer';
+import { AudioPlayerContextAudioProvider } from '@lib/AudioPlayerContextAudioProvider/AudioPlayerContextAudioProvider';
+import { AudioPlayerVisualizerWaveform } from '@lib/AudioPlayerVisualizerWaveform/AudioPlayerVisualizerWaveform';
+import { AudioPlayerVisualizerFrequencyBars } from '@lib/AudioPlayerVisualizerFrequencyBars/AudioPlayerVisualizerFrequencyBars';
 
 // Create the namespace with Object.assign inline for each component
 export const AudioPlayerCompoundComponent = {
   Root: Object.assign(AudioPlayer, { displayName: 'AudioPlayer.Root' }),
   Provider: Object.assign(AudioPlayerContextProvider, { displayName: 'AudioPlayer.Provider' }),
+  AudioContextProvider: Object.assign(AudioPlayerContextAudioProvider, {
+    displayName: 'AudioPlayer.AudioContextProvider',
+  }),
   Author: Object.assign(AudioPlayerAuthor, { displayName: 'AudioPlayer.Author' }),
   Controls: Object.assign(AudioPlayerControls, { displayName: 'AudioPlayer.Controls' }),
   Image: Object.assign(AudioPlayerImage, { displayName: 'AudioPlayer.Image' }),
@@ -41,4 +47,10 @@ export const AudioPlayerCompoundComponent = {
     displayName: 'AudioPlayer.ControlShuffle',
   }),
   ControlLoop: Object.assign(AudioPlayerControlLoop, { displayName: 'AudioPlayer.ControlLoop' }),
+  VisualizerWaveform: Object.assign(AudioPlayerVisualizerWaveform, {
+    displayName: 'AudioPlayer.VisualizerWaveform',
+  }),
+  VisualizerFrequencyBars: Object.assign(AudioPlayerVisualizerFrequencyBars, {
+    displayName: 'AudioPlayer.VisualizerFrequencyBars',
+  }),
 };
