7b315c44092655550ae24753b83bc572cbd66c97
gdominguez210 <gdominguez210@live.com>
2025-05-04 22:42:35 -0400

    refactor(lib/AudioPlayerTime/useAudioPlayerTime): refactor to use abstracted formatDurationForDisplay

--- lib/AudioPlayerTime/useAudioPlayerTime.ts
+++ lib/AudioPlayerTime/useAudioPlayerTime.ts
@@ -1,29 +1,10 @@
-import { intervalToDuration } from 'date-fns';
-
-function isDefined<T>(argument: T | undefined): argument is T {
-  return argument !== undefined;
-}
-
-export function formatAudioDurationForDisplay(audioDurationInSeconds: number = 0) {
-  const {
-    hours,
-    minutes = 0,
-    seconds = 0,
-  } = intervalToDuration({
-    start: 0,
-    end: audioDurationInSeconds * 1000,
-  });
-
-  const formatWithZero = (num: number) => String(num).padStart(2, '0');
-
-  return [hours, minutes, seconds].filter(isDefined).map(formatWithZero).join(':');
-}
+import { formatDurationForDisplay } from '@lib/utils/formatDurationForDisplay/formatDurationForDisplay';
 
 export function useAudioPlayerTime(props: { currentTime: number; duration: number }) {
   const { currentTime, duration } = props;
 
   return {
-    currentTimeDisplay: formatAudioDurationForDisplay(currentTime),
-    durationDisplay: formatAudioDurationForDisplay(duration),
+    currentTimeDisplay: formatDurationForDisplay(currentTime),
+    durationDisplay: formatDurationForDisplay(duration),
   };
 }
