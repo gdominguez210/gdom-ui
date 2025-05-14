fb8afb4a33e0305a78f95ca8229114b9e468ea50
gdominguez210 <gdominguez210@live.com>
2025-05-13 22:12:49 -0400

    docs(.storybook/preview): add sorting for documentation sections

---
+++ .storybook/preview.tsx
@@ -0,0 +1,42 @@
+import type { Preview } from '@storybook/react';
+import './storybook.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    docs: {
+      story: {
+        inline: true,
+      },
+    },
+    options: {
+      storySort: {
+        method: 'alphabetical',
+        order: [
+          'Overview',
+          ['Introduction', 'Getting Started', 'Releases'],
+          'Core Concepts',
+          [
+            'Composability Over Configuration',
+            'Two-Tier Components',
+            'Polymorphic Components',
+            'Custom Hooks',
+            'Styling Approach',
+            'Accessibility',
+            'Code Splitting',
+          ],
+          '*',
+        ],
+      },
+    },
+  },
+
+  tags: ['autodocs'],
+};
+
+export default preview;
