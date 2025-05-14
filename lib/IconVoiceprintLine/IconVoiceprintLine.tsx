8d75924bf27627a0e22fcc64c1f2f235f4239f7d
gdominguez210 <gdominguez210@live.com>
2025-05-13 21:58:51 -0400

    feat(lib/IconVoiceprintLine): add IconVoiceprintLine

---
+++ lib/IconVoiceprintLine/IconVoiceprintLine.tsx
@@ -0,0 +1,6 @@
+import { ReactComponent as VoiceprintLineSvg } from '@lib/assets/svgs/voiceprint-line.svg';
+import { Icon, type IconProps } from '@lib/Icon/Icon';
+
+export function IconVoiceprintLine(props: Omit<IconProps, 'as'>) {
+  return <Icon as={VoiceprintLineSvg} {...props} />;
+}
