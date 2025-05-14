23eb9db2d4052cf51d3ba3a76d176152fc6a9d14
gdominguez210 <gdominguez210@live.com>
2025-05-13 21:38:27 -0400

    feat(lib/IconCloseFill): add IconDiscFill

---
+++ lib/IconDiscFill/IconDiscFill.tsx
@@ -0,0 +1,6 @@
+import { ReactComponent as DiscFillSvg } from '@lib/assets/svgs/disc-fill.svg';
+import { Icon, type IconProps } from '@lib/Icon/Icon';
+
+export function IconDiscFill(props: Omit<IconProps, 'as'>) {
+  return <Icon as={DiscFillSvg} {...props} />;
+}
