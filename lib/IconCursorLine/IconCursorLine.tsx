e36a8e5fc7dc176c334f8f5bb7b7a9fb4908733d
gdominguez210 <gdominguez210@live.com>
2025-05-13 21:38:12 -0400

    feat(lib/IconCursorLine): add IconCursorLine

---
+++ lib/IconCursorLine/IconCursorLine.tsx
@@ -0,0 +1,6 @@
+import { ReactComponent as CursorLineSvg } from '@lib/assets/svgs/cursor-line.svg';
+import { Icon, type IconProps } from '@lib/Icon/Icon';
+
+export function IconCursorLine(props: Omit<IconProps, 'as'>) {
+  return <Icon as={CursorLineSvg} {...props} />;
+}
