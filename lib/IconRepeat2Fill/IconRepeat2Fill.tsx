4bb148a76cecce2699ee1ff8518ff58c6d831df4
gdominguez210 <gdominguez210@live.com>
2025-05-13 21:55:39 -0400

    feat(lib/IconRepeat2Fill): add IconRepeat2Fill

---
+++ lib/IconRepeat2Fill/IconRepeat2Fill.tsx
@@ -0,0 +1,6 @@
+import { ReactComponent as Repeat2FillSvg } from '@lib/assets/svgs/repeat-2-fill.svg';
+import { Icon, type IconProps } from '@lib/Icon/Icon';
+
+export function IconRepeat2Fill(props: Omit<IconProps, 'as'>) {
+  return <Icon as={Repeat2FillSvg} {...props} />;
+}
