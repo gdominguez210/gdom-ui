1208f099870ce021f5c63e3dd10f6d2cb7cc8728
gdominguez210 <gdominguez210@live.com>
2025-05-13 21:57:40 -0400

    feat(lib/IconSpeedFill): add IconSpeedFill

---
+++ lib/IconSpeedFill/IconSpeedFill.tsx
@@ -0,0 +1,6 @@
+import { ReactComponent as SpeedFillSvg } from '@lib/assets/svgs/speed-fill.svg';
+import { Icon, type IconProps } from '@lib/Icon/Icon';
+
+export function IconSpeedFill(props: Omit<IconProps, 'as'>) {
+  return <Icon as={SpeedFillSvg} {...props} />;
+}
