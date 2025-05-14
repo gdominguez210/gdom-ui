221fcdf4e9957bfeebe6faf910991833c8bb00aa
gdominguez210 <gdominguez210@live.com>
2025-05-13 21:39:27 -0400

    feat(lib/IconFontSize): add IconFontSize

---
+++ lib/IconFontSize/IconFontSize.tsx
@@ -0,0 +1,6 @@
+import { ReactComponent as FontSizeSvg } from '@lib/assets/svgs/font-size.svg';
+import { Icon, type IconProps } from '@lib/Icon/Icon';
+
+export function IconFontSize(props: Omit<IconProps, 'as'>) {
+  return <Icon as={FontSizeSvg} {...props} />;
+}
