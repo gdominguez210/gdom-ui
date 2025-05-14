fac1687475bdea0ff01f2a4f9f9549fc76ddfe76
gdominguez210 <gdominguez210@live.com>
2025-05-13 21:56:28 -0400

    feat(lib/IconRewindFill): add IconRewindFill

---
+++ lib/IconRewindFill/IconRewindFill.tsx
@@ -0,0 +1,6 @@
+import { ReactComponent as RewindFillSvg } from '@lib/assets/svgs/rewind-fill.svg';
+import { Icon, type IconProps } from '@lib/Icon/Icon';
+
+export function IconRewindFill(props: Omit<IconProps, 'as'>) {
+  return <Icon as={RewindFillSvg} {...props} />;
+}
