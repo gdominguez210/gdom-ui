49e8bed696a0bfb2260f899c79a0735e786590da
gdominguez210 <gdominguez210@live.com>
2025-05-16 23:50:20 -0400

    docs(.storybook/components/Canvas): add Canvas component

---
+++ .storybook/components/Canvas/Canvas.tsx
@@ -0,0 +1,23 @@
+import { type ComponentPropsWithRef } from 'react';
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+type CanvasProps = ComponentPropsWithRef<'div'>;
+
+export function Canvas(props: CanvasProps) {
+  const { children, className, ...rest } = props;
+
+  return (
+    <section
+      className={twMerge(
+        clsx(
+          'mt-[24px] mb-[40px] rounded-md border-[1px_1px_1px] border-[rgba(38,85,115,0.15)] bg-white p-[34px] shadow-[0px_1px_3px_0px_rgba(38,85,115,0.15)]',
+          className,
+        ),
+      )}
+      data-custom-canvas
+      {...rest}
+    >
+      {children}
+    </section>
+  );
+}
