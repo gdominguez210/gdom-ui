0e0bf60b2c59324a22946f95666fac78ef0057aa
gdominguez210 <gdominguez210@live.com>
2025-05-17 21:48:01 -0400

    docs(.storybook/decorators/CollapseCategory): add decorator to collapse argType categories by default

---
+++ .storybook/decorators/CollapseCategory/CollapseCategory.tsx
@@ -0,0 +1,28 @@
+import { StoryFn } from '@storybook/react';
+import { Decorator } from '@storybook/types';
+import { useEffect } from 'react';
+
+export const CollapseCategory =
+  (...categoryNames: string[]): Decorator =>
+  (Story: StoryFn) => {
+    useEffect(() => {
+      setTimeout(() => {
+        // Process each category
+        categoryNames.forEach((categoryName) => {
+          // Find the <tr> with a title starting with "Hide <category>"
+          const tr = Array.from(document.querySelectorAll('tr')).find((tr) =>
+            tr.getAttribute('title')?.startsWith(`Hide ${categoryName}`),
+          );
+
+          // Find the first button inside that <tr>
+          const button = tr?.querySelector('button[tabindex="0"]');
+
+          if (button) {
+            (button as HTMLElement).click();
+          }
+        });
+      }, 0);
+    }, []);
+
+    return Story();
+  };
