73238f919e51a8f5d1c8ec9373d87ffb33b9636e
gdominguez210 <gdominguez210@live.com>
2025-05-19 21:02:54 -0400

    feat: use multiple entry-points for library bundle, so each component has their own barrel file for code-splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,35 @@
-/// <reference types="vitest" />
-import { join, resolve } from 'node:path';
-import react from '@vitejs/plugin-react-swc';
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { globSync } from 'glob';
+import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { peerDependencies } from './package.json';
 import svgr from 'vite-plugin-svgr';
 import tailwindcss from '@tailwindcss/vite';
+import { peerDependencies } from './package.json';
+
+// Dynamically find all barrel files
+const entryPoints = globSync('./lib/*/index.ts').reduce((entries, path) => {
+  // Extract the folder name from the path
+  const pathArray = path.split('/');
+  const name = pathArray[1];
+  entries[name] = resolve(__dirname, path);
+  return entries;
+}, {});
+
+//Add the main entry point
+entryPoints['index'] = resolve(__dirname, 'lib/index.ts');
 
 export default defineConfig({
   plugins: [
     react(),
-    dts({ rollupTypes: true }), // Output .d.ts files
+    dts({
+      entryRoot: 'lib',
+      outDir: 'dist',
+      exclude: ['**/*.test.ts', '**/*.test.tsx', '**/*.stories.tsx', '**/*.mdx'],
+    }), // Output .d.ts files
     tsconfigPaths(),
     svgr({
       include: '**/*.svg',
@@ -27,19 +45,33 @@ export default defineConfig({
   build: {
     target: 'esnext',
     minify: false,
+    cssCodeSplit: false,
     lib: {
-      entry: resolve(__dirname, join('lib', 'index.ts')),
-      fileName: 'index',
+      entry: entryPoints,
       formats: ['es', 'cjs'],
+      fileName: (format, entryName) => {
+        if (entryName === 'index') {
+          return `index.${format}.js`;
+        }
+
+        return `${entryName}/index.${format}.js`;
+      },
     },
     rollupOptions: {
-      // Exclude peer dependencies from the bundle to reduce bundle size
       external: ['react/jsx-runtime', ...Object.keys(peerDependencies)],
+      output: {
+        exports: 'named',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        chunkFileNames: 'chunks/[name]-[hash].js',
+      },
     },
   },
   test: {
     environment: 'happy-dom',
-    setupFiles: './lib/test/setup.ts',
+    setupFiles: './test/setup.ts',
     coverage: {
       all: false,
       enabled: false,
