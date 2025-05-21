19b7d59907f5a4818a26d8e14faee986de65bf7e
gdominguez210 <gdominguez210@live.com>
2025-05-20 22:40:26 -0400

    style(.storybook/components/UsageNote): add shadow

--- .storybook/components/UsageNote/UsageNote.tsx
+++ .storybook/components/UsageNote/UsageNote.tsx
@@ -5,7 +5,7 @@ export function UsageNote(props: ComponentPropsWithRef<'section'>) {
 
   return (
     <section
-      className="font-inter mt-[24px] mb-[40px] rounded-md border border-slate-400 bg-slate-100 p-3 text-slate-700"
+      className="font-inter mt-[24px] mb-[40px] rounded-md border border-slate-400 bg-slate-100 p-3 text-slate-700 shadow-[0px_1px_3px_0px_rgba(38,85,115,0.15)]"
       {...props}
     >
       <span className="font-bold">Usage Note</span>
