626bf0b1a1f52d91e1bb5c87bb3b32d787fcb6e6
gdominguez210 <gdominguez210@live.com>
2025-05-23 22:06:41 -0400

    docs(.storybook/components/Copyright): add Copyright component

---
+++ .storybook/components/Copyright/Copyright.tsx
@@ -0,0 +1,36 @@
+import { type ComponentPropsWithRef } from 'react';
+import { Button } from '@/lib/Button/Button';
+import clsx from 'clsx';
+
+const START_YEAR = 2024;
+const CURRENT_YEAR = new Date().getFullYear();
+const AUTHOR = 'Gary Dominguez';
+const LICENSE_URL = 'https://github.com/gdominguez210/gdom-ui/blob/main/LICENSE.md';
+
+export type CopyrightProps = ComponentPropsWithRef<'div'>;
+
+export function Copyright(props: CopyrightProps) {
+  const { className, ...rest } = props;
+  const year = CURRENT_YEAR > START_YEAR ? `${START_YEAR} - ${CURRENT_YEAR}` : START_YEAR;
+
+  return (
+    <div
+      className={clsx('flex items-center gap-2 text-sm text-slate-500', className)}
+      {...rest}
+    >
+      <span>
+        © {year} {AUTHOR}
+      </span>
+      |
+      <Button
+        variant="linkColor"
+        as="a"
+        href={LICENSE_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        License
+      </Button>
+    </div>
+  );
+}
