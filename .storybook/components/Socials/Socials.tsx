ce61c3535271f7d39cf86671444054cb7fc58888
gdominguez210 <gdominguez210@live.com>
2025-05-23 22:07:12 -0400

    docs(.storybook/components/Socials): add Socials component

---
+++ .storybook/components/Socials/Socials.tsx
@@ -0,0 +1,43 @@
+import { type ComponentPropsWithRef } from 'react';
+import { Button } from '@/lib/Button/Button';
+import { IconLibrary } from '@/lib/IconLibrary/IconLibrary';
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export type SocialsProps = ComponentPropsWithRef<'div'>;
+
+export function Socials(props: SocialsProps) {
+  const { className, ...rest } = props;
+
+  return (
+    <div
+      className={twMerge(clsx('flex items-center', className))}
+      {...rest}
+    >
+      <Button
+        variant="linkGray"
+        size="lg"
+        as="a"
+        href="https://github.com/gdominguez210/gdom-ui"
+        iconOnly
+        aria-label="GitHub"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconLibrary name="github-fill" />
+      </Button>
+      <Button
+        variant="linkGray"
+        as="a"
+        size="lg"
+        href="https://www.linkedin.com/in/gary-dominguez/"
+        iconOnly
+        aria-label="LinkedIn"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconLibrary name="linkedin-box-fill" />
+      </Button>
+    </div>
+  );
+}
