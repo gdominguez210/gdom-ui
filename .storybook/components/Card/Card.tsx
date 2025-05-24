5519a7c51ac0c74193c4133295796e7ca8cf1c08
gdominguez210 <gdominguez210@live.com>
2025-05-23 22:06:27 -0400

    docs(.storybook/components/Card): add Card component

---
+++ .storybook/components/Card/Card.tsx
@@ -0,0 +1,91 @@
+import type { ComponentPropsWithRef, ElementType } from 'react';
+import clsx from 'clsx';
+import { IconLibrary } from '@/lib/IconLibrary/IconLibrary';
+import type { IconName } from '@/lib/IconLibrary/data';
+import { Polymorphic } from '@/lib/Polymorphic/Polymorphic';
+
+type CardRootProps<T extends ElementType = 'div'> = ComponentPropsWithRef<T> & {
+  as?: T;
+};
+
+function CardRoot<T extends ElementType = 'div'>(props: CardRootProps<T>) {
+  const { as = 'div', children, className, ...rest } = props;
+
+  return (
+    <Polymorphic
+      as={as}
+      className={clsx(
+        'sb-unstyled relative flex flex-col items-center gap-4 rounded-md border-[1px_1px_1px] border-[rgba(38,85,115,0.15)] bg-white p-[34px] shadow-[0px_1px_3px_0px_rgba(38,85,115,0.15)]',
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </Polymorphic>
+  );
+}
+
+type CardHeaderProps<T extends ElementType = 'h2'> = ComponentPropsWithRef<T> & {
+  as?: T;
+};
+
+function CardHeader<T extends ElementType = 'h2'>(props: CardHeaderProps<T>) {
+  const { as = 'h2', children, className, ...rest } = props;
+
+  return (
+    <Polymorphic
+      as={as}
+      className={clsx('text-2xl font-bold text-balance', className)}
+      {...rest}
+    >
+      {children}
+    </Polymorphic>
+  );
+}
+
+type CardContentProps<T extends ElementType = 'div'> = ComponentPropsWithRef<T> & {
+  as?: T;
+};
+
+function CardContent<T extends ElementType = 'div'>(props: CardContentProps<T>) {
+  const { as = 'div', children, className, ...rest } = props;
+
+  return (
+    <Polymorphic
+      as={as}
+      className={clsx('leading-7 text-balance text-gray-500', className)}
+      {...rest}
+    >
+      {children}
+    </Polymorphic>
+  );
+}
+
+type CardIconProps<T extends ElementType = 'div'> = ComponentPropsWithRef<T> & {
+  as?: T;
+  name: IconName;
+};
+
+function CardIcon<T extends ElementType = 'div'>(props: CardIconProps<T>) {
+  const { as = 'div', name, className, ...rest } = props;
+
+  return (
+    <Polymorphic
+      as={as}
+      className={clsx(
+        'inline-block rounded-full bg-blue-200 p-4 text-6xl text-white shadow-lg shadow-blue-900/20 *:text-blue-700',
+        className,
+      )}
+      {...rest}
+    >
+      <IconLibrary name={name} />
+    </Polymorphic>
+  );
+}
+
+export const Card = {
+  Root: CardRoot,
+  Header: CardHeader,
+  Content: CardContent,
+  Icon: CardIcon,
+};
