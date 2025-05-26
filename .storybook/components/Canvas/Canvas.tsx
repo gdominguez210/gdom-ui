82dae7685b94e7298ed879cbc1f2fc111bbb7885
gdominguez210 <gdominguez210@live.com>
2025-05-26 16:14:42 -0400

    refactor(.storybook/components/Canvas): refactor to use Polymorphic

--- .storybook/components/Canvas/Canvas.tsx
+++ .storybook/components/Canvas/Canvas.tsx
@@ -1,13 +1,16 @@
-import type { ComponentPropsWithRef } from 'react';
+import type { ComponentPropsWithRef, ElementType } from 'react';
 import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
-type CanvasProps = ComponentPropsWithRef<'div'>;
+import { Polymorphic, type PolymorphicProps } from '@/lib/Polymorphic/Polymorphic';
 
-export function Canvas(props: CanvasProps) {
-  const { children, className, ...rest } = props;
+export type CanvasProps<T extends ElementType = 'div'> = PolymorphicProps<T>;
+
+export function Canvas<T extends ElementType = 'div'>(props: CanvasProps<T>) {
+  const { as = 'section', children, className, ...rest } = props;
 
   return (
-    <section
+    <Polymorphic
+      as={as}
       className={twMerge(
         clsx(
           'mt-[24px] mb-[40px] rounded-md border-[1px_1px_1px] border-[rgba(38,85,115,0.15)] bg-white p-[34px] shadow-[0px_1px_3px_0px_rgba(38,85,115,0.15)]',
@@ -18,6 +21,6 @@ export function Canvas(props: CanvasProps) {
       {...rest}
     >
       {children}
-    </section>
+    </Polymorphic>
   );
 }
