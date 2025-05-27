0d9c690932721844405ccace80f28912862896ac
gdominguez210 <gdominguez210@live.com>
2025-05-26 21:42:37 -0400

    chore(lib/AudioVisualizerFrequencyBars.stories): fix imports

--- lib/AudioVisualizerFrequencyBars/AudioVisualizerFrequencyBars.stories.tsx
+++ lib/AudioVisualizerFrequencyBars/AudioVisualizerFrequencyBars.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { StoryObj, Meta } from '@storybook/react-vite';
 import { AudioVisualizerFrequencyBars } from '@/lib/AudioVisualizerFrequencyBars/AudioVisualizerFrequencyBars';
 import { trackData } from '@/lib/AudioPlayer/data';
 import { AudioPlayerCompoundComponent as AudioPlayer } from '@/lib/AudioPlayer/namespace';
