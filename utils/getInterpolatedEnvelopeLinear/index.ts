1698fcda48f0a67565f9e28df0aa934d9232a606
gdominguez210 <gdominguez210@live.com>
2025-06-17 23:34:51 -0400

    refactor(utils/getInterpolatedEnvelopeLinear): update type

--- utils/getInterpolatedEnvelopeLinear/index.ts
+++ utils/getInterpolatedEnvelopeLinear/index.ts
@@ -9,7 +9,7 @@ import { findEnvelopeInSampleRange } from '@/utils/findEnvelopeInSampleRange';
  * @param options - Sampling options
  */
 export function getInterpolatedEnvelopeLinear(
-  data: number[],
+  data: number[] | Float32Array,
   exactIndex: number,
   options?: EnvelopeSampleOptions,
 ): EnvelopeSegment {
