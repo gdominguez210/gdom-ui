f9f830a936ff0461699ceae425dfbde69fa48c83
gdominguez210 <gdominguez210@live.com>
2025-06-29 13:09:09 -0400

    refactor(utils/getInterpolatedvalueLinear): handle edge-cases

--- utils/getInterpolatedValueLinear/index.ts
+++ utils/getInterpolatedValueLinear/index.ts
@@ -10,8 +10,15 @@ export function getInterpolatedValueLinear(
   data: number[] | Float32Array,
   exactIndex: number,
 ): number {
-  const y1 = data[Math.floor(exactIndex)] ?? 0;
-  const y2 = data[Math.min(data.length - 1, Math.floor(exactIndex) + 1)] ?? 0;
+  if (data.length === 0) {
+    return 0;
+  }
+
+  const index1 = Math.max(0, Math.min(data.length - 1, Math.floor(exactIndex)));
+  const index2 = Math.max(0, Math.min(data.length - 1, Math.floor(exactIndex) + 1));
+
+  const y1 = data[index1]!;
+  const y2 = data[index2]!;
 
   const fraction = exactIndex - Math.floor(exactIndex);
 
