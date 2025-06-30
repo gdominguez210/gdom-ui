2befb38ea2bc4012a1a4fcad68da363f0d820a06
gdominguez210 <gdominguez210@live.com>
2025-06-29 23:41:16 -0400

    chore(data/peakOverviewData): fix types

--- data/peakOverviewData.ts
+++ data/peakOverviewData.ts
@@ -1,6 +1,6 @@
 import { audioData as trackOneAudioData } from '@/data/audio/58730401-c910-4a77-935e-83d71d5d1a52';
-import type { EnvelopeSegmentInfo } from '@/types/audio';
+import type { EnvelopeSegment } from '@/types/audio';
 
-export const overViewData: Record<string, EnvelopeSegmentInfo[]> = {
-  '58730401-c910-4a77-935e-83d71d5d1a52': trackOneAudioData.data[2].peaks,
+export const overViewData: Record<string, EnvelopeSegment[]> = {
+  '58730401-c910-4a77-935e-83d71d5d1a52': trackOneAudioData.data[2]!.peaks,
 };
