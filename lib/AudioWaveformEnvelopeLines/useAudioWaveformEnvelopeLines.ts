a71a2a367da931a21c4cb9dd88f7b3f8debd5825
gdominguez210 <gdominguez210@live.com>
2025-07-03 23:31:32 -0400

    fix(lib/AudioWaveformEnvelopeLines/useAudioWaveformEnvelopeLines): add calculateSegments to drawWaveform useEffect dependency array to redraw waveform if segment data has changed

--- lib/AudioWaveformEnvelopeLines/useAudioWaveformEnvelopeLines.ts
+++ lib/AudioWaveformEnvelopeLines/useAudioWaveformEnvelopeLines.ts
@@ -223,7 +223,7 @@ export function useAudioWaveformEnvelopeLines(
     if (isReady && canvasRef.current && drawOnCanvasReady) {
       drawWaveform();
     }
-  }, [isReady, canvasRef, drawOnCanvasReady, drawWaveform, currentColor]);
+  }, [isReady, canvasRef, drawOnCanvasReady, drawWaveform, currentColor, calculateSegments]);
 
   return { canvasRef: setCanvasRef, drawWaveform, calculateSegments, handleResize };
 }
