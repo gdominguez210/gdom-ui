02a0d42db9f84266b2b244b1ac6164f4420e94cf
gdominguez210 <gdominguez210@live.com>
2025-07-09 22:58:50 -0400

    chore(utils/interpolateOKLCH): move source code to index

--- utils/interpolateOKLCH/index.test.ts
+++ utils/interpolateOKLCH/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { interpolateOKLCH } from './interpolateOKLCH';
+import { interpolateOKLCH } from '@/utils/interpolateOKLCH';
 import type { OKLCHColor } from '@/types/colors';
 
 describe('interpolateOKLCH', () => {
