406ca882fed29fd1bb8cb4687d68c703ebac0cea
gdominguez210 <gdominguez210@live.com>
2025-07-13 02:01:25 -0400

    refactor(lib/AudioPlayerInfo): use Polymorphic component as base

--- lib/AudioPlayerInfo/AudioPlayerInfo.tsx
+++ lib/AudioPlayerInfo/AudioPlayerInfo.tsx
@@ -1,27 +1,25 @@
-import type { ComponentPropsWithRef, ElementType } from 'react';
-import { twMerge } from 'tailwind-merge';
+import type { ElementType } from 'react';
 import clsx from 'clsx';
+import { Polymorphic, type PolymorphicProps } from '@/lib/Polymorphic/Polymorphic';
 
 /**
  * Props for the track information container component
  */
-export type AudioPlayerInfoProps<T extends ElementType = 'div'> = {
-  /** Element to render as @default div */
-  as?: T;
-} & ComponentPropsWithRef<T>;
+export type AudioPlayerInfoProps<T extends ElementType = 'div'> = PolymorphicProps<T>;
 
 /**
  * Container component for displaying track information (image, title, artist)
  */
-export function AudioPlayerInfo<T extends ElementType>(props: AudioPlayerInfoProps<T>) {
-  const { as: Element = 'div', className, children, ...restProps } = props;
+export function AudioPlayerInfo<T extends ElementType = 'div'>(props: AudioPlayerInfoProps<T>) {
+  const { as = 'div', className, children, ...restProps } = props;
 
   return (
-    <Element
-      className={twMerge(clsx('flex items-center gap-4', className))}
+    <Polymorphic
+      as={as}
+      className={clsx('flex', 'items-center', 'gap-4', className)}
       {...restProps}
     >
       {children}
-    </Element>
+    </Polymorphic>
   );
 }
