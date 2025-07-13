9f4d3c396572297fa69e65666dbb9d84ef331b91
gdominguez210 <gdominguez210@live.com>
2025-07-13 02:01:06 -0400

    refactor(lib/AudioPlayerControls): use Polymorphic component as base

--- lib/AudioPlayerControls/AudioPlayerControls.tsx
+++ lib/AudioPlayerControls/AudioPlayerControls.tsx
@@ -1,26 +1,34 @@
-import type { ComponentPropsWithRef, ElementType } from 'react';
-import { twMerge } from 'tailwind-merge';
+import type { ElementType } from 'react';
 import clsx from 'clsx';
+import { Polymorphic, type PolymorphicProps } from '@/lib/Polymorphic/Polymorphic';
 /**
  * Props for the playback controls container
  */
-export type AudioPlayerControlsProps<T extends ElementType = 'div'> = {
-  /** Element to render as @default div */
-  as?: T;
-} & ComponentPropsWithRef<T>;
+export type AudioPlayerControlsProps<T extends ElementType = 'div'> = PolymorphicProps<T>;
 
 /**
  * Base component for laying out audio player controls
  */
-export function AudioPlayerControls<T extends ElementType>(props: AudioPlayerControlsProps<T>) {
-  const { as: Element = 'div', children, className, ...restProps } = props;
+export function AudioPlayerControls<T extends ElementType = 'div'>(
+  props: AudioPlayerControlsProps<T>,
+) {
+  const { as = 'div', children, className, ...restProps } = props;
 
   return (
-    <Element
-      className={twMerge(clsx('flex items-center justify-center gap-1 p-4 text-2xl', className))}
+    <Polymorphic
+      as={as}
+      className={clsx(
+        'flex',
+        'items-center',
+        'justify-center',
+        'gap-1',
+        'p-4',
+        'text-2xl',
+        className,
+      )}
       {...restProps}
     >
       {children}
-    </Element>
+    </Polymorphic>
   );
 }
