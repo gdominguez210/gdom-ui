ae5828f13b71c9a5416dfa0afb5d568090bc04e9
gdominguez210 <gdominguez210@live.com>
2025-07-19 23:36:11 -0400

    chore(lib/AudioWaveformProgress/AudioWaveformProgressPrimitive): use cn util

--- lib/AudioWaveformProgress/AudioWaveformProgressPrimitive.tsx
+++ lib/AudioWaveformProgress/AudioWaveformProgressPrimitive.tsx
@@ -2,8 +2,7 @@ import {
   CanvasResponsive,
   type CanvasResponsiveProps,
 } from '@/lib/CanvasResponsive/CanvasResponsive';
-import { twMerge } from 'tailwind-merge';
-import clsx from 'clsx';
+import { cn } from '@/utils/cn';
 
 export type AudioWaveformProgressPrimitiveProps = CanvasResponsiveProps;
 
@@ -13,11 +12,9 @@ export function AudioWaveformProgressPrimitive(props: AudioWaveformProgressPrimi
   return (
     <CanvasResponsive
       {...restProps}
-      className={twMerge(
-        clsx(
-          'relative cursor-pointer bg-radial from-neutral-50 from-0% to-neutral-100 to-90% before:absolute before:inset-0 before:bg-radial before:from-white before:to-transparent before:bg-[size:1px_1px] before:content-[""]',
-          className,
-        ),
+      className={cn(
+        'relative cursor-pointer bg-radial from-neutral-50 from-0% to-neutral-100 to-90% before:absolute before:inset-0 before:bg-radial before:from-white before:to-transparent before:bg-[size:1px_1px] before:content-[""]',
+        className,
       )}
     />
   );
