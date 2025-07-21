9be6157e14e0d8a164664805373f6d12d7ae79b7
gdominguez210 <gdominguez210@live.com>
2025-07-20 22:05:15 -0400

    refactor(.storybook/components/ViewSource): add support for automatic ViewSource url, that’s overrideable via props

--- .storybook/components/ViewSource/ViewSource.tsx
+++ .storybook/components/ViewSource/ViewSource.tsx
@@ -1,5 +1,6 @@
 import { Button, type ButtonProps } from '@/lib/Button/Button';
 import { IconLibrary } from '@/lib/IconLibrary/IconLibrary';
+import { useOf } from '@storybook/addon-docs/blocks';
 
 const BASE_URL = 'https://github.com/gdominguez210/gdom-ui/tree/main/lib/';
 
@@ -8,7 +9,10 @@ export type ViewSourceProps = Omit<ButtonProps<'a'>, 'as' | 'iconOnly'>;
 export function ViewSource(props: ViewSourceProps) {
   const { className, href, ...restProps } = props;
 
-  const composedUrl = `${BASE_URL}${href}`;
+  const resolved = useOf('component');
+  const component = resolved?.type === 'component' ? resolved.component : null;
+
+  const composedUrl = `${BASE_URL}${href ?? component?.displayName}`;
 
   return (
     <Button
