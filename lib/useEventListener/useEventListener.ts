36092aaac3c90c2e223cda3d829306f658ffee47
gdominguez210 <gdominguez210@live.com>
2025-07-23 23:30:00 -0400

    refactor(lib/useEventListener): add support for cleanup function

--- lib/useEventListener/useEventListener.ts
+++ lib/useEventListener/useEventListener.ts
@@ -10,14 +10,30 @@ export type UseEventListenerOptions<T extends Event = Event> = {
   event: string;
   handler: EventListenerHandler<T>;
   options?: boolean | AddEventListenerOptions;
+  cleanup?: () => void;
 };
 
+/**
+ * Hook that adds an event listener to a target element with automatic cleanup
+ * Primarily designed for global objects like window, document, etc.
+ *
+ * @param options - The options for the event listener
+ * @param options.target - The target element to add the event listener to
+ * @param options.event - The event to listen for
+ * @param options.handler - The handler function to call when the event is triggered
+ * @param options.options - The options for the event listener
+ * @param options.cleanup - A function to call when the event listener is removed
+ */
+
 export function useEventListener<T extends Event = Event>(options: UseEventListenerOptions<T>) {
-  const { target, event, handler, options: eventOptions } = options;
+  const { target, event, handler, options: eventOptions, cleanup } = options;
 
   const handlerRef = useLatest(handler);
+  const cleanupRef = useLatest(cleanup);
 
   useEffect(() => {
+    const currentCleanup = cleanupRef.current;
+
     const eventHandler = (event: Event) => {
       const currentHandler = handlerRef.current;
 
@@ -32,6 +48,7 @@ export function useEventListener<T extends Event = Event>(options: UseEventListe
 
     return () => {
       target.removeEventListener(event, eventHandler, eventOptions);
+      currentCleanup?.();
     };
-  }, [target, event, handlerRef, eventOptions]);
+  }, [target, event, handlerRef, eventOptions, cleanupRef]);
 }
