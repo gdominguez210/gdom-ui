de839d81a86add721645ceb332683c577a740b6b
gdominguez210 <gdominguez210@live.com>
2025-07-31 23:43:48 -0400

    style(.storybook/components/TableOfContents): adjust padding

--- .storybook/components/TableOfContents/TableOfContents.tsx
+++ .storybook/components/TableOfContents/TableOfContents.tsx
@@ -42,7 +42,7 @@ export function TableOfContents() {
   if (items.length === 0) return null;
 
   return (
-    <StyledTableOfContents className="sb-unstyled fixed top-3 right-4 z-1000 flex max-h-[calc(100vh-var(--spacing)*6)] w-48 flex-col gap-1 rounded-lg border-2 border-slate-200 bg-white shadow-xs">
+    <StyledTableOfContents className="sb-unstyled fixed top-3 right-4 z-1000 flex max-h-[calc(100vh-var(--spacing)*6)] w-48 flex-col rounded-lg border-2 border-slate-200 bg-white shadow-xs">
       <Button
         className="flex justify-between font-bold text-inherit"
         variant="tertiary"
@@ -61,7 +61,7 @@ export function TableOfContents() {
         className="scrollbar-thin scrollbar-thumb-blue-100 scrollbar-track-transparent overflow-y-auto"
       >
         <nav
-          className={cn('flex flex-col gap-1 text-base transition-all duration-200', {
+          className={cn('flex flex-col gap-1 py-1 text-base transition-all duration-200', {
             flex: isOpen,
             hidden: !isOpen,
           })}
